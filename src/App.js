acf22d4e2686884d762f3f067aaf90ed7acd2561
Amandha W. Barok <amandha.barok@gmail.com>
2021-07-26 13:05:11 -0700

    First requiriment done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import './App.css';
+import Table from './components/Table';
+import Context from './context/Context';
 
 function App() {
-  return (
-    <span>Hello, App!</span>
-  );
+  const [planets, setPlanets] = useState([]);
+
+  // didMount
+  useEffect(() => {
+    const getPlanet = async () => {
+      const endpoint = `https://swapi-trybe.herokuapp.com/api/planets/`
+      const data = await fetch(endpoint).then((data) => data.json());
+      // console.log(data.results);
+      setPlanets(data.results);
+    }
+
+    getPlanet();
+  }, []);
+    return (
+      <Context.Provider value={planets}>
+        { planets.length && <Table /> }
+      </Context.Provider>
+    );
 }
 
 export default App;
