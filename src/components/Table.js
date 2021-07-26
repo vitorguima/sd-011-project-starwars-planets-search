7e057b9c8439d78ac0bcd960923b503c80cd0ab0
Tales Coelho dos Santos <tales-coelho@live.com>
2021-07-26 17:46:42 -0300

    Requisito 3 Completo
    Requisito 3

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Context from '../context/Context';
 
 export default function Table() {
@@ -7,25 +7,80 @@ export default function Table() {
       filterByName: {
         name: '',
       },
+      filterByNumericValues: [],
     },
   });
+  const [filterNumeric, setFilterNumeric] = useState({
+    column: '',
+    comparison: '',
+    value: 0,
+  });
+  const [filteredPlanets, setFilteredPlanets] = useState(null);
   const { data } = useContext(Context);
+
+  useEffect(() => {
+    setFilteredPlanets(data.filter((planet) => planet.name
+      .includes(filterState.filters.filterByName.name)));
+  }, [data, filterState.filters.filterByName]);
+
+  useEffect(() => {
+    const { filters } = filterState;
+    const { filterByNumericValues } = filters;
+    // console.log(filterByNumericValues.length - 1);
+    const teste = filterByNumericValues[filterByNumericValues.length - 1] || '';
+    switch (teste.comparison) {
+    case 'maior que':
+      setFilteredPlanets(data
+        .filter((planet) => Number(planet[teste.column]) > Number(teste.value)));
+      break;
+    case 'menor que':
+      setFilteredPlanets(data
+        .filter((planet) => Number(planet[teste.column]) < Number(teste.value)));
+      break;
+    case 'igual a':
+      setFilteredPlanets(data
+        .filter((planet) => Number(planet[teste.column]) === Number(teste.value)));
+      break;
+    default:
+      break;
+    }
+  }, [filterState.filters.filterByNumericValues]);
+
   if (!data[0]) {
     return (<p>Carregando...</p>);
   }
   const filteredValues = Object.keys(data[0]).filter((value) => value !== 'residents');
-  const filteredPlanets = data.filter((planet) => planet.name
-    .includes(filterState.filters.filterByName.name));
-  // console.log(filteredPlanets);
+
+  const searchNumeric = () => {
+    const filterP = filteredPlanets.filter((value) => (
+      value[filterNumeric.column] > filterNumeric.value));
+    setFilteredPlanets(filterP);
+    setFilterState({
+      ...filterState,
+      filters: {
+        ...filterState.filters,
+        filterByNumericValues: [...filterState.filters.filterByNumericValues,
+          filterNumeric],
+      },
+    });
+    setFilterNumeric({
+      column: '',
+      comparison: '',
+      value: '',
+    });
+  };
+
   return (
     <div>
       <label htmlFor="filterPlanets">
         <input
           id="filterPlanets"
+          type="text"
+          name="name"
           data-testid="name-filter"
           onChange={ (({ target }) => setFilterState({
-            ...filterState,
             filters: {
+              ...filterState.filters,
               filterByName: {
                 name: target.value,
               },
@@ -33,6 +88,54 @@ export default function Table() {
           })) }
         />
       </label>
+      <select
+        data-testid="column-filter"
+        value={ filterNumeric.column }
+        onChange={ (({ target }) => setFilterNumeric(
+          { ...filterNumeric,
+            column: target.value,
+          },
+        )) }
+      >
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
+      <select
+        data-testid="comparison-filter"
+        value={ filterNumeric.comparison }
+        onChange={ (({ target }) => setFilterNumeric(
+          { ...filterNumeric,
+            comparison: target.value,
+          },
+        )) }
+      >
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+      <label htmlFor="valueFilter">
+        <input
+          type="number"
+          id="valueFilter"
+          data-testid="value-filter"
+          value={ filterNumeric.value }
+          onChange={ (({ target }) => setFilterNumeric(
+            { ...filterNumeric,
+              value: target.value,
+            },
+          )) }
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ searchNumeric }
+      >
+        Filtrar
+      </button>
       <table>
         <thead>
           <tr>
