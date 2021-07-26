dcbc4f4688cc5dc4deb39f4b931ad3ff391ec382
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-07-26 14:42:45 -0300

    Estrutura a base para os componentes iniciais

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Table from './components/Table';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <>
+      <Header />
+      <Table />
+    </>
   );
 }
 
---
+++ src/components/Table.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Table() {
+  return (
+    <div>
+      Tabela
+    </div>
+  );
+}
+
+export default Table;
