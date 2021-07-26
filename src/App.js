6486cae0ee87e4e7d468c3d96424887a967d0e70
Marco Galindo <marco1408@gmail.com>
2021-07-26 16:54:25 -0300

    requisito 2 finalizado

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
 
   const data = {
     planets,
+    setPlanets,
     loading,
   };
 
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,35 +1,51 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 import TableRow from './TableRow';
 
 export default function Table() {
   const { planets, loading } = useContext(PlanetsContext);
-  // const [data, setData] = useState({
-  //   name: [],
-  // });
+  const [filters, setFilters] = useState({
+    filterByName: {
+      name: '',
+    },
+  });
 
   // useEffect(() => {
-  //   setData(planets.map((planet) => ({
-  //     name: planet.name,
-  //   })));
-  // }, [planets]);
+  //   const { filterByName: { name } } = filters;
+  //   if (name !== '') {
+  //     const planetsList = planets.filter((planet) => planet.name === name);
+  //     setPlanets(planetsList);
+  //   } else {
+  //     setPlanets(planets);
+  //   }
+  // }, [filters]);
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
   const tableHead = Object.keys(planets[0]).filter((key) => key !== 'residents');
+  const { filterByName: { name } } = filters;
+  const planetsList = planets.filter((planet) => planet.name.includes(name));
 
   return (
-    <table>
-      <thead>
-        <tr>
-          { tableHead.map((key, index) => <th key={ index }>{key}</th>) }
-        </tr>
-      </thead>
-      <tbody>
-        { planets.map((planet, index) => <TableRow key={ index } planet={ planet } />) }
-      </tbody>
-    </table>
+    <div>
+      <input
+        type="text"
+        data-testid="name-filter"
+        onChange={ (e) => setFilters({ filterByName: { name: e.target.value } }) }
+      />
+      <table>
+        <thead>
+          <tr>
+            { tableHead.map((key, index) => <th key={ index }>{key}</th>) }
+          </tr>
+        </thead>
+        <tbody>
+          { planetsList.map((planet,
+            index) => <TableRow key={ index } planet={ planet } />) }
+        </tbody>
+      </table>
+    </div>
   );
 }
