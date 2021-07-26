5d42e7b72fd3dad4fa336c7fc98ea6e4214d8ca8
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-07-26 15:34:47 -0300

    Add input to filter by name to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import PlanetsProvider from './context/PlanetsProvider';
+import Filters from './components/Filters';
 import Table from './components/PlanetsTable';
 
 function App() {
   return (
     <PlanetsProvider>
       <span>Project StarWars Planets Search!</span>
+      <Filters />
       <Table />
     </PlanetsProvider>
   );
