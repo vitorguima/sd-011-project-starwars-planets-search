cf60d361e00a579e0bfaaa2bc345f7f618056da8
Amandha W. Barok <amandha.barok@gmail.com>
2021-07-26 13:08:05 -0700

    Fixing lint errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Table from './components/Table';
 import Context from './context/Context';
@@ -9,19 +9,19 @@ function App() {
   // didMount
   useEffect(() => {
     const getPlanet = async () => {
-      const endpoint = `https://swapi-trybe.herokuapp.com/api/planets/`
-      const data = await fetch(endpoint).then((data) => data.json());
+      const endpoint = 'https://swapi-trybe.herokuapp.com/api/planets/';
+      const data = await fetch(endpoint).then((results) => results.json());
       // console.log(data.results);
       setPlanets(data.results);
-    }
+    };
 
     getPlanet();
   }, []);
-    return (
-      <Context.Provider value={planets}>
-        { planets.length && <Table /> }
-      </Context.Provider>
-    );
+  return (
+    <Context.Provider value={ planets }>
+      { planets.length && <Table /> }
+    </Context.Provider>
+  );
 }
 
 export default App;
--- src/components/Table.js
+++ src/components/Table.js
@@ -5,53 +5,51 @@ import Context from '../context/Context';
 function Table() {
   return (
     <Context.Consumer>
-      {(planets) => {
-        return (
-          <table>
-            <thead>
-              <tr>
-                <th>Nome</th>
-                <th>Período de Rotação</th>
-                <th>Perpiodo Orbital</th>
-                <th>Diâmetro</th>
-                <th>Clima</th>
-                <th>Gravidade</th>
-                <th>Terreno</th>
-                <th>Superfície da Água</th>
-                <th>População</th>
-                <th>Filmes</th>
-                <th>Criado</th>
-                <th>Editado</th>
-                <th>Url</th>
-              </tr>
-            </thead>
-            <tbody>
-            {planets.map(planet=> (
-              <tr>
+      {(planets) => (
+        <table>
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>Período de Rotação</th>
+              <th>Perpiodo Orbital</th>
+              <th>Diâmetro</th>
+              <th>Clima</th>
+              <th>Gravidade</th>
+              <th>Terreno</th>
+              <th>Superfície da Água</th>
+              <th>População</th>
+              <th>Filmes</th>
+              <th>Criado</th>
+              <th>Editado</th>
+              <th>Url</th>
+            </tr>
+          </thead>
+          <tbody>
+            {planets.map((planet) => (
+              <tr key={ planet.name }>
                 <td>{planet.name}</td>
                 <td>{planet.rotation_period}</td>
                 <td>{ planet.orbital_period }</td>
-               <td>{ planet.diameter }</td>
-               <td>{ planet.climate }</td>
-               <td>{ planet.gravity }</td>
-               <td>{ planet.terrain }</td>
-               <td>{ planet.surface_water }</td>
-               <td>{ planet.population }</td>
-               <td>{ planet.films }</td>
-               <td>{ planet.created }</td>
-               <td>{ planet.edited }</td>
-               <td>{ planet.url }</td>
+                <td>{ planet.diameter }</td>
+                <td>{ planet.climate }</td>
+                <td>{ planet.gravity }</td>
+                <td>{ planet.terrain }</td>
+                <td>{ planet.surface_water }</td>
+                <td>{ planet.population }</td>
+                <td>{ planet.films }</td>
+                <td>{ planet.created }</td>
+                <td>{ planet.edited }</td>
+                <td>{ planet.url }</td>
               </tr>
             ))}
-            </tbody>
-          </table>
-          // <ul>
-          //   {
-          //     planets.map(({ name }) => <li>{name}</li>)
-          //   }
-          // </ul>
-        )
-      }}
+          </tbody>
+        </table>
+        // <ul>
+        //   {
+        //     planets.map(({ name }) => <li>{name}</li>)
+        //   }
+        // </ul>
+      )}
     </Context.Consumer>
   );
 }
