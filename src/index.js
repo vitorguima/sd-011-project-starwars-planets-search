84a4a09bc00191f3542228661afe0c734ce07dc8
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-07-26 15:04:34 -0300

    Adiciona o provider ao src/index.js, englobando o <App />

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AppProvider from './ContextAPI_Configs/AppProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <AppProvider>
+    <App />
+  </AppProvider>,
+  document.getElementById('root'),
+);
