cfe7c8abcb51cff646615b3823b451c26591e358
Julio Filizzola <juliofilizzola@hotmail.com>
2021-07-26 15:12:35 -0300

    renderizando o Table no app com o Global context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Table from './components/Table';
+import GlobalContext from './Context/GlobalContext';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <GlobalContext>
+      <Table />
+    </GlobalContext>
   );
 }
 
