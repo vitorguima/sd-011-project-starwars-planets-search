fc9a40a60edbb25af13567a53fbab2418c778c2d
tomsantos07 <xotmsantosx@live.com>
2021-07-26 20:44:48 -0300

    construçao de parte da tabela, com base na api

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import Context from '../context/Context';
 
 function Table() {
   return (
-     
-  )
+    <Context.Consumer>
+      {( data ) => (
+        <table>
+          <tr>
+            <th>Nome</th>
+            <th>Período de Rotação</th>
+            <th>Período Orbital</th>
+            <th>Diâmetro</th>
+            <th>Clima</th>
+            <th>Gravidade</th>
+            <th>Terreno</th>
+            <th>Água da Superfície</th>
+            <th>População</th>
+            <th>Filmes</th>
+            <th>Criado</th>
+            <th>Editado</th>
+            <th>URL</th>
+          </tr>
+        </table>
+      )}
+    </Context.Consumer>
+  );
 }
+
+export default Table;
