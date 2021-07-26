d41dea23f201829ab987854ad0c9f82ab527a9ab
heyset <c.inaciomatheus@gmail.com>
2021-07-26 16:45:55 -0300

    Adiciona elementos do DOM

--- src/Components/Controls.jsx
+++ src/Components/Controls.jsx
@@ -4,6 +4,19 @@ import { usePlanets } from '../hooks/usePlanets';
 function Controls() {
   const [textFilter, setTextFilter] = useState('');
   const { addFilter } = usePlanets();
+  const NUMERIC_FIELDS = [
+    { translation: 'População', value: 'population' },
+    { translation: 'Período Orbital', value: 'orbital_period' },
+    { translation: 'Diâmetro', value: 'diameter' },
+    { translation: 'Período de Rotação', value: 'rotation_period' },
+    { translation: 'Água na Superfície', value: 'surface_water' },
+  ];
+
+  const COMPARE_OPTIONS = [
+    { translation: 'maior que', value: 'gt' },
+    { translation: 'igual a', value: 'eq' },
+    { translation: 'menor que', value: 'lt' },
+  ];
 
   function handleAddFilter({ type, content }) {
     const filters = {
@@ -16,26 +29,53 @@ function Controls() {
     addFilter(filters[type]);
   }
 
+  const mapSelect = (options) => options.map(({ translation, value }) => (
+    <option
+      key={ value }
+      value={ value }
+    >
+      { translation }
+    </option>
+  ));
+
   return (
     <section>
-      <form
-        onSubmit={ (e) => {
-          e.preventDefault();
-          handleAddFilter({ type: 'text' });
-        } }
-      >
+      <fieldset>
+        <legend>Filtrar por nome</legend>
         <input
           data-testid="name-filter"
           type="text"
-          placeholder="Filtrar por nome"
+          placeholder="Insira o nome ou parte dele"
           value={ textFilter }
           onChange={ ({ target }) => {
             setTextFilter(target.value);
             handleAddFilter({ type: 'text', content: target.value });
           } }
         />
+      </fieldset>
 
-        <button type="submit">Adicionar filtro de texto</button>
+      <form>
+        <fieldset>
+          <legend>Filtrar por números</legend>
+          <select name="numeric_field" data-testid="column-filter" required>
+            { mapSelect(NUMERIC_FIELDS) }
+          </select>
+          <select name="" data-testid="comparison-filter" required>
+            { mapSelect(COMPARE_OPTIONS) }
+          </select>
+          <input
+            type="number"
+            placeholder="30000"
+            data-testid="value-filter"
+            required
+          />
+          <button
+            type="submit"
+            data-testid="button-filter"
+          >
+            Adicionar Filtro Numérico
+          </button>
+        </fieldset>
       </form>
     </section>
   );
