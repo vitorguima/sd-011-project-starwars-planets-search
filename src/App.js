76c4c22f0f89d8f0448ef5961a4020e4b4b7fa34
Amandha W. Barok <amandha.barok@gmail.com>
2021-07-26 14:39:37 -0700

    Thrith requiriment done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Context from './context/Context';
 function App() {
   const [planets, setPlanets] = useState([]);
   const [filterByName, setFilterByName] = useState('');
+  const [filterByFirstDropdown, setFirstDropdown] = useState('population');
+  const [filterBysecondDropdown, setSecondDropdown] = useState('maior que');
+  const [filterByNumber, setFilterByNumber] = useState(null);
+  const [filteredPlanets, setFilteredPlanets] = useState([]);
 
   // didMount
   useEffect(() => {
@@ -14,23 +18,87 @@ function App() {
       const data = await fetch(endpoint).then((results) => results.json());
       // console.log(data.results);
       setPlanets(data.results);
+      setFilteredPlanets(data.results);
     };
 
     getPlanet();
   }, []);
 
-  function handlePlanetFiltered({ target: { value } }) {
+  function handlePlanetFilteredByName({ target: { value } }) {
     setFilterByName(value);
+    const newFilter = planets.filter((item) => item.name.includes(value));
+    setFilteredPlanets(newFilter);
   }
-  const filteredPlanets = planets.filter((item) => item.name.includes(filterByName));
+
+  function handleFirstDropdown({ target: { value } }) {
+    setFirstDropdown(value);
+  }
+
+  function handleSecondDropDown({ target: { value } }) {
+    setSecondDropdown(value);
+  }
+
+  function handleFilterByNumber({ target: { value } }) {
+    setFilterByNumber(parseInt(value, 10));
+  }
+
+  function handleButtonFilter() {
+    let newFilter;
+    if (filterBysecondDropdown === 'maior que') {
+      newFilter = planets.filter((item) => parseInt(item[filterByFirstDropdown], 10)
+       > filterByNumber);
+    } else if (filterBysecondDropdown === 'menor que') {
+      newFilter = planets.filter((item) => parseInt(item[filterByFirstDropdown], 10)
+        < filterByNumber);
+    } else if (filterBysecondDropdown === 'igual a') {
+      newFilter = planets.filter((item) => parseInt(item[filterByFirstDropdown], 10)
+       === filterByNumber);
+    }
+    setFilteredPlanets(newFilter);
+  }
+
   return (
     <div>
       <input
         data-testid="name-filter"
         type="text"
-        onChange={ handlePlanetFiltered }
+        onChange={ handlePlanetFilteredByName }
         value={ filterByName }
       />
+
+      <select
+        data-testid="column-filter"
+        onChange={ handleFirstDropdown }
+      >
+        <option>population</option>
+        <option>orbital_period</option>
+        <option>diameter</option>
+        <option>rotation_period</option>
+        <option>surface_water</option>
+      </select>
+
+      <select
+        data-testid="comparison-filter"
+        onChange={ handleSecondDropDown }
+      >
+        <option>maior que</option>
+        <option>menor que</option>
+        <option>igual a</option>
+      </select>
+
+      <input
+        type="number"
+        data-testid="value-filter"
+        onChange={ handleFilterByNumber }
+      />
+
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ handleButtonFilter }
+      >
+        Aplicar filtro
+      </button>
       <Context.Provider
         value={ {
           data: filteredPlanets,
