4a274415c04271a75ff64520f6b0df1ce6bfda3d
Julio Filizzola <juliofilizzola@hotmail.com>
2021-07-26 15:18:23 -0300

    criando um state no componente table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,6 +3,14 @@ import Context from '../Context/Context';
 
 function Table() {
   const resultsApi = React.useContext(Context);
+  const [filters, setFilter] = React.useState({
+    filters: {
+      filterByName: {
+        name: '',
+      },
+    },
+  });
+
   return (
     <div>
       olá
