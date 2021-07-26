85aa01f21a3ece6ec1d9ea18ec16ed32ea9ac324
Julio Filizzola <juliofilizzola@hotmail.com>
2021-07-26 15:08:12 -0300

    criando Globalcontext

---
+++ src/Context/GlobalContext.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Context from './Context';
+
+const GlobalContext = ({ children }) => {
+  const [data, setData] = React.useState();
+
+  React.useEffect(() => {
+    fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+      .then((results) => results.json())
+      .then((json) => setData(json));
+  }, []);
+
+  return (
+    <Context.Provider value={ data }>
+      {children}
+    </Context.Provider>
+  );
+};
+
+export default GlobalContext;
+
+GlobalContext.propTypes = {
+  children: PropTypes.element.isRequired,
+};
