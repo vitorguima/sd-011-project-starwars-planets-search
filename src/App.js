ba862d82d18e25cfe60d5d840904de2dda584d13
marlon307 <marlon-ramosb@hotmail.com>
2021-07-26 16:29:58 -0300

    Rq 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import MyContext from './MyContext';
+import Table from './Table';
 
 function App() {
+  const [initstate, setInitState] = useState([]);
+
+  async function getApi() {
+    const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+    const { results } = await response.json();
+    setInitState(results);
+  }
+
+  useEffect(() => {
+    getApi();
+  }, []);
+
   return (
-    <span>Hello, App!</span>
+    <MyContext.Provider value={ initstate }>
+      <Table />
+    </MyContext.Provider>
+
   );
 }
 
