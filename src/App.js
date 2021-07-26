416d5ec17dc206001ef5a43698f0deec566d279f
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-07-26 15:09:33 -0300

    Primeiro Requisito Feito.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Table from './Components/Table';
+import PlanetsProvider from './context/PlanetsProvider.js';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <PlanetsProvider>
+      <Table />
+    </PlanetsProvider>
   );
 }
 
