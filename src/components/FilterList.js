eee0d2a6223906a3855feebd0793e0026d9109ed
Arthur Hermann <arthurhermannc@gmail.com>
2021-07-26 16:10:31 -0300

    add filter list

---
+++ src/components/FilterList.js
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+import SWContext from '../context/SWContext';
+
+// RETURN A LIST OF FILTERS THAT HAS BEEN APPLIED AT THE CURRENT MOMENT. EACH FILTER CAN BE REMOVED CLICKING IN THE X BUTTON
+function FilterList() {
+  const { filters, setFilters,
+    columnsOptions, setColumnsOptions } = useContext(SWContext);
+  const { filterByNumericValues } = filters;
+
+  const removeFilter = (column) => {
+    setColumnsOptions([...columnsOptions, column]);
+    setFilters({ ...filters,
+      filterByNumericValues: filterByNumericValues
+        .filter((filter) => filter.column !== column) });
+  };
+
+  return (
+    <div>
+      Filtros
+      {filterByNumericValues
+        .map(({ column }, i) => (
+          <div data-testid="filter" key={ i }>
+            { column }
+            <button
+              type="button"
+              onClick={ () => removeFilter(column) }
+            >
+              X
+            </button>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default FilterList;
