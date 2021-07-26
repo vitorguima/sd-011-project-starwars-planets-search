e9d941528f011c534eb28f9052ecab3e8e48b687
jamneto <netomoura22@gmail.com>
2021-07-26 18:48:07 -0300

    cumprimento do requsito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import Table from './components/Table';
+import PlanetsProvider from './context/PlanetsProvider';
 
 function App() {
   return (
-    <span>Hello, App!!</span>
+    <PlanetsProvider>
+      <div>
+        <Table />
+      </div>
+    </PlanetsProvider>
   );
 }
 
