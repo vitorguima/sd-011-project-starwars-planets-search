a27832120fc50425cf3b89b1a239adc2214911ca
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-07-26 15:48:03 -0300

    Requisito 01: Criando arquivos iniciais do context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Table from './components/Table';
 
 function App() {
   return (
     <div>
       <h1>Projeto StarWars</h1>
+      <Table />
     </div>
   );
 }
---
+++ src/components/Table.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Table() {
+  return (
+    <div>
+      <h2>Componente Tabela</h2>
+    </div>
+  );
+}
+
+export default Table;
