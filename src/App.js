f3f62bcca76121471101beab8fa399b47a0394ef
Marco Galindo <marco1408@gmail.com>
2021-07-26 20:21:54 -0300

    requisito 3 finalizado

--- src/App.js
+++ src/App.js
@@ -5,28 +5,72 @@ import PlanetsContext from './context/PlanetsContext';
 
 function App() {
   const [planets, setPlanets] = useState([]);
+  const [filteredPlanets, setFilteredPlanets] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [column, setColumn] = useState('');
+  const [comparison, setComparison] = useState('');
+  const [value, setValue] = useState(0);
+  const [filters, setFilters] = useState({
+    filterByName: {
+      name: '',
+    },
+    filterByNumericValues: [],
+  });
 
   useEffect(() => {
     const getPlanets = async () => {
       const endpoint = 'https://swapi-trybe.herokuapp.com/api/planets/';
       const { results } = await fetch(endpoint).then((data) => data.json());
       setPlanets(results);
+      setFilteredPlanets(results);
       setLoading(false);
     };
 
     getPlanets();
   }, []);
 
+  useEffect(() => {
+    let newList = planets.filter((e) => e.name.includes(filters.filterByName.name));
+    if (filters.filterByNumericValues.length > 0) {
+      filters.filterByNumericValues.forEach(({ comparison: cm, column: c, value: v }) => {
+        switch (cm) {
+        case 'maior que':
+          newList = planets.filter((planet) => Number(planet[c]) > Number(v));
+          break;
+        case 'menor que':
+          newList = planets.filter((planet) => Number(planet[c]) < Number(v));
+          break;
+        case 'igual a':
+          newList = planets.filter((planet) => Number(planet[c]) === Number(v));
+          break;
+        default:
+          console.log('choose a proper comparison');
+        }
+      });
+    }
+    setFilteredPlanets(newList);
+  }, [filters]);
+
+  const addFilter = () => {
+    const newFilter = { column, comparison, value };
+    setFilters({
+      ...filters, filterByNumericValues: [...filters.filterByNumericValues, newFilter] });
+  };
+
   const data = {
     planets,
-    setPlanets,
+    filteredPlanets,
     loading,
+    filters,
+    setFilters,
+    setColumn,
+    setComparison,
+    setValue,
   };
 
   return (
     <PlanetsContext.Provider value={ data }>
-      <Table />
+      <Table addFilter={ addFilter } />
     </PlanetsContext.Provider>
   );
 }
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,32 +1,41 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import PlanetsContext from '../context/PlanetsContext';
 import TableRow from './TableRow';
 
-export default function Table() {
-  const { planets, loading } = useContext(PlanetsContext);
-  const [filters, setFilters] = useState({
-    filterByName: {
-      name: '',
-    },
-  });
-
-  // useEffect(() => {
-  //   const { filterByName: { name } } = filters;
-  //   if (name !== '') {
-  //     const planetsList = planets.filter((planet) => planet.name === name);
-  //     setPlanets(planetsList);
-  //   } else {
-  //     setPlanets(planets);
-  //   }
-  // }, [filters]);
+export default function Table({ addFilter }) {
+  const {
+    planets,
+    loading,
+    setFilters,
+    filteredPlanets, setColumn, setComparison, setValue } = useContext(PlanetsContext);
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
   const tableHead = Object.keys(planets[0]).filter((key) => key !== 'residents');
-  const { filterByName: { name } } = filters;
-  const planetsList = planets.filter((planet) => planet.name.includes(name));
+  // const { filterByName: { name } } = filters;
+  // let planetsList = planets.filter((planet) => planet.name.includes(name));
+
+  // const addFilter = () => {
+  // const newFilter = { column, comparison, value };
+  // setFilters({
+  //   ...filters, filterByNumericValues: [...filters.filterByNumericValues, newFilter] });
+  // switch (comparison) {
+  // case 'greater':
+  //   planetsList = planetsList.filter((planet) => planet[column] > value);
+  //   break;
+  // case 'smaller':
+  //   planetsList = planetsList.filter((planet) => planet[column] < value);
+  //   break;
+  // case 'equal':
+  //   planetsList = planetsList.filter((planet) => planet[column] === value);
+  //   break;
+  // default:
+  //   console.log('choose a proper comparison');
+  // }
+  // };
 
   return (
     <div>
@@ -35,6 +44,39 @@ export default function Table() {
         data-testid="name-filter"
         onChange={ (e) => setFilters({ filterByName: { name: e.target.value } }) }
       />
+      <select
+        data-testid="column-filter"
+        name="column"
+        onChange={ (e) => setColumn(e.target.value) }
+      >
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
+      <select
+        data-testid="comparison-filter"
+        name="comparison"
+        onChange={ (e) => setComparison(e.target.value) }
+      >
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+      <input
+        type="number"
+        data-testid="value-filter"
+        name="value"
+        onChange={ (e) => setValue(e.target.value) }
+      />
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ addFilter }
+      >
+        Add filter
+      </button>
       <table>
         <thead>
           <tr>
@@ -42,10 +84,14 @@ export default function Table() {
           </tr>
         </thead>
         <tbody>
-          { planetsList.map((planet,
+          { filteredPlanets.map((planet,
             index) => <TableRow key={ index } planet={ planet } />) }
         </tbody>
       </table>
     </div>
   );
 }
+
+Table.propTypes = {
+  addFilter: PropTypes.func.isRequired,
+};
