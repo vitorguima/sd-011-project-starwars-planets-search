43d75059ae5935cf0333bff86ef927abc7a0827c
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-07-26 17:19:12 -0300

    req 1 - lint

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -6,25 +6,27 @@ function Table() {
   const { data } = useContext(PlanetsContext);
   return (
     <table id="table">
-      <tr id="head">
-        <th>name</th>
-        <th>rotation_period</th>
-        <th>orbital_period</th>
-        <th>diameter</th>
-        <th>climate</th>
-        <th>gravity</th>
-        <th>terrain</th>
-        <th>surface_water</th>
-        <th>population</th>
-        <th>films</th>
-        <th>created</th>
-        <th>edited</th>
-        <th>url</th>
-      </tr>
-      <tr id="body">
+      <thead id="head">
+        <tr>
+          <th>name</th>
+          <th>rotation_period</th>
+          <th>orbital_period</th>
+          <th>diameter</th>
+          <th>climate</th>
+          <th>gravity</th>
+          <th>terrain</th>
+          <th>surface_water</th>
+          <th>population</th>
+          <th>films</th>
+          <th>created</th>
+          <th>edited</th>
+          <th>url</th>
+        </tr>
+      </thead>
+      <tbody id="body">
         {data && (
           data.map((planet, index) => (
-            <div key={ index } id="block">
+            <tr key={ index }>
               <td>{planet.name}</td>
               <td>{planet.rotation_period}</td>
               <td>{planet.orbital_period}</td>
@@ -34,16 +36,14 @@ function Table() {
               <td>{planet.terrain}</td>
               <td>{planet.surface_water}</td>
               <td>{planet.population}</td>
-              <div id="list">
-                {planet.films
-                  .map((film, numberKey) => <td key={ numberKey }>{film}</td>)}
-              </div>
+              {planet.films
+                .map((film, numberKey) => <td key={ numberKey }>{film}</td>)}
               <td>{planet.created}</td>
               <td>{planet.edited}</td>
               <td>{planet.url}</td>
-            </div>
+            </tr>
           )))}
-      </tr>
+      </tbody>
     </table>
   );
 }
