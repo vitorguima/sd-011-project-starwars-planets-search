f51a200be286708cf3b3f89b65d93dc99fecea7c
jhenriquemargrafm <jh.margraf@gmail.com>
2021-07-26 17:27:52 -0300

    Requisito 1 - fetch, useEffect e tabela montada

---
+++ src/APIcontext/Provider.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Context from './Context';
+
+function Provider({ children }) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchPlanets = async () => {
+      const fetchAPI = fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+      const response = await fetchAPI;
+      const { results } = await response.json();
+      setData(results);
+    };
+    fetchPlanets();
+  }, []);
+
+  return (
+    <Context.Provider value={ { data } }>
+      { children }
+    </Context.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default Provider;
--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Provider from './APIcontext/Provider';
+import Table from './components/Table';
 
 function App() {
   return (
-    <span>Hello, let's go!</span>
+    <Provider>
+      <Table />
+    </Provider>
   );
 }
 
