360009390d2ec7fb84435f4ac8a1d5da280c6286
tomsantos07 <xotmsantosx@live.com>
2021-07-26 23:09:59 -0300

    continuando com o componente table

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,23 +4,45 @@ import Context from '../context/Context';
 function Table() {
   return (
     <Context.Consumer>
-      {( data ) => (
+      {(data) => (
         <table>
-          <tr>
-            <th>Nome</th>
-            <th>Período de Rotação</th>
-            <th>Período Orbital</th>
-            <th>Diâmetro</th>
-            <th>Clima</th>
-            <th>Gravidade</th>
-            <th>Terreno</th>
-            <th>Água da Superfície</th>
-            <th>População</th>
-            <th>Filmes</th>
-            <th>Criado</th>
-            <th>Editado</th>
-            <th>URL</th>
-          </tr>
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>Período de Rotação</th>
+              <th>Período Orbital</th>
+              <th>Diâmetro</th>
+              <th>Clima</th>
+              <th>Gravidade</th>
+              <th>Terreno</th>
+              <th>Água da Superfície</th>
+              <th>População</th>
+              <th>Filmes</th>
+              <th>Criado</th>
+              <th>Editado</th>
+              <th>URL</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((planet) => (
+              <tr key={ planet.name }>
+                <td>{planet.name}</td>
+                <td>{planet.rotation_period}</td>
+                <td>{planet.orbital_period}</td>
+                <td>{planet.diameter}</td>
+                <td>{planet.climate}</td>
+                <td>{planet.gravity}</td>
+                <td>{planet.terrain}</td>
+                <td>{planet.surface_water}</td>
+                <td>{planet.population}</td>
+                <td>{planet.films}</td>
+                <td>{planet.created}</td>
+                <td>{planet.edited}</td>
+                <td>{planet.url}</td>
+              </tr>
+            ))}
+          </tbody>
+
         </table>
       )}
     </Context.Consumer>
