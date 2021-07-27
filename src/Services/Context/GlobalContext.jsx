0f9a6c11142afedb368d79e0baf66483dd62402c
Vinicius Gonçalves <viniciusfod@gmail.com>
2021-07-27 02:23:35 -0300

    lint

--- src/Services/Context/GlobalContext.jsx
+++ src/Services/Context/GlobalContext.jsx
@@ -5,20 +5,19 @@ import useFetch from '../Hooks/useFetch';
 export const Context = React.createContext();
 
 const GlobalContext = ({ children }) => {
-    const {data, loading, error, request } = useFetch();
-    return (
-        <Context.Provider value={{ data, loading, error, request }}>
-            {children}
-        </Context.Provider>
-    );
+  const { data, loading, error, request } = useFetch();
+  return (
+    <Context.Provider value={ { data, loading, error, request } }>
+      {children}
+    </Context.Provider>
+  );
 };
 
-
 GlobalContext.propTypes = {
-    children: PropTypes.oneOfType([
-      PropTypes.arrayOf(PropTypes.node),
-      PropTypes.node,
-    ]).isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
 };
 
 export default GlobalContext;
