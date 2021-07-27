411374a7aef7c1550a96b18e37d768604a5a3839
Leandro Lopes <leandro_carlos@uol.com.br>
2021-07-27 12:01:40 -0300

    resolvendo stylelint

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -6,57 +6,42 @@ function Table() {
   console.log(data);
   return (
     <table>
-      <tr>
-        <th>name</th>
-        <th>climated</th>
-        <th>diameter</th>
-        <th>edited</th>
-        <th>gravity</th>
-        <th>orbital-period</th>
-        <th>population</th>
-        <th>rotation-period</th>
-        <th>surface-water</th>
-        <th>terrain</th>
-        <th>url</th>
-        <th>films</th>
-        <th>created</th>
-      </tr>
-
-      { data.map((iten, index) => (
-        <tr key={ index }>
-          <td>{iten.name}</td>
-          <td>{iten.climate}</td>
-          <td>{iten.diameter}</td>
-          <td>{iten.edited}</td>
-          <td>
-            {iten.gravity}
-          </td>
-          <td>
-            {iten.orbital_period}
-          </td>
-          <td>
-            {iten.population}
-          </td>
-          <td>
-            {iten.rotation_period}
-          </td>
-          <td>
-            {iten.surface_water}
-          </td>
-          <td>
-            {iten.terrain}
-          </td>
-          <td>
-            {iten.url}
-          </td>
-          <td>
-            {iten.films}
-          </td>
-          <td>
-            {iten.created}
-          </td>
+      <thead>
+        <tr>
+          <th>name</th>
+          <th>climated</th>
+          <th>diameter</th>
+          <th>edited</th>
+          <th>gravity</th>
+          <th>orbital-period</th>
+          <th>population</th>
+          <th>rotation-period</th>
+          <th>surface-water</th>
+          <th>terrain</th>
+          <th>url</th>
+          <th>films</th>
+          <th>created</th>
         </tr>
-      ))}
+      </thead>
+      <tbody>
+        { data.map((item) => (
+          <tr key={ item.name }>
+            <td>{item.name}</td>
+            <td>{item.climate}</td>
+            <td>{item.diameter}</td>
+            <td>{item.edited}</td>
+            <td>{item.gravity}</td>
+            <td>{item.orbital_period}</td>
+            <td>{item.population}</td>
+            <td>{item.rotation_period}</td>
+            <td>{item.surface_water}</td>
+            <td>{item.terrain}</td>
+            <td>{item.url}</td>
+            <td>{item.films}</td>
+            <td>{item.created}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
 
   );
