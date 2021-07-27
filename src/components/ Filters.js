f806337ec39ea820a7b6b11123ca9fc52a13b014
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-07-26 21:54:06 -0300

    Set logic in filters component to remove column option.

--- src/components/ Filters.js
+++ src/components/ Filters.js
@@ -1,41 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PlanetsContext from '../contexts/PlanetsContext';
 // Duvida: Não estou controlando o input de pesquisa dinamica e fazendo direto a alteração do provider. Correto isso?
 
-// const context = {
-//   data,
-//   column,
-//   comparison,
-//   value,
-//   filtered,
-//   filters,
-//   setData,
-//   setName,
-//   setColumn,
-//   setComparison,
-//   setValue,
-//   setFiltered,
-//   setFilters,
-// };
-//   filters:
-//     {
-//       filterByName: {
-//         name: '',
-//       },
-//       filterByNumericValues: [
-//         {
-//           column: 'population',
-//           comparison: 'maior que',
-//           value: '100000',
-//         },
-//       ],
-//     },
-// };
-// const MAIOR_QUE = 'maior que';
-// const MENOR_QUE = 'menor que';
-// const IGUAL_A = 'igual a';
-
 export default function Filter() {
+  // criar uma logica melhor para filtragem das opções de coluna
+  const [columnsOptions, setColumnsOptions] = useState([
+    'population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water',
+  ]);
+
+  // function setComunsOptions(columnFilter) {
+  //   columnsOptions = columnsOptions.filter((item) => item !== columnFilter);
+  // }
+
   const {
     data,
     column,
@@ -59,10 +35,6 @@ export default function Filter() {
     ));
     setFiltered(planetsByInput);
   }
-  // Primeiro - Criar um codigo que filtre a compinação. Ou seja, primeiro seta os valores que serao filtrados.
-  // Extrair os filtros corretos. O ultimo que foi colocado. O filtro não pode sobrepor o que ja tem lah.
-  // Uma vez separado o que será filtrado fazer função que monta esse filtro utilizando um map que utiliza os valores de filtro para fazer a seleção.
-  // Apos feita a filtragem setar os filtered com a nova lista.
 
   function planetsByFilters() {
     const newFilters = {
@@ -70,72 +42,10 @@ export default function Filter() {
       filterByNumericValues: [
         ...filters.filterByNumericValues, { column, comparison, value }],
     };
-
+    setColumnsOptions(columnsOptions.filter((item) => item !== column));
     setFilters(newFilters);
-    console.log('Rodou ate setFilter');
-    // if (filters.filterByNumericValues.length > 0) {
-    //   const newfiltered = [];
-    //   filters.forEach((filter) => {
-    //     switch (filter.comparison) {
-    //     case MAIOR_QUE:
-    //       newfiltered.push(filtered.filter((planet) => planet[filter.column] > value));
-    //       console.log('rodou maior');
-    //       break;
-    //     case MENOR_QUE:
-    //       newfiltered.push(filtered.filter((planet) => planet[filter.column] < value));
-    //       console.log('rodou menor');
-    //       break;
-    //     case IGUAL_A:
-    //       newfiltered.push(filtered.filter((planet) => planet[filter.column] === value));
-    //       console.log('rodou igual');
-    //       break;
-    //     default:
-    //       newfiltered.push(filtered);
-    //       console.log('Deu b.o no comparasion');
-    //       break;
-    //     }
-    //   });
-    //   setFiltered(newfiltered);
-    // }
   }
-  // function planetsByFilters() {
-  //   const newFilters = {
-  //     ...filters,
-  //     filterByNumericValues: [
-  //       ...filters.filterByNumericValues, { column, comparison, value }],
-  //   };
-  //   setFilters(newFilters);
-  //   // Filtar novamente o data e setar novo filtered
-  //   // let filteredListPlanets = [];
-  //   filters.filterByNumericValues.forEach((filter, index) => {
-  //     if (index > 0) {
-  //       const result = filtered.map((planet) => {
-  //         // planet[column] {`comparison`}
-  //         // if (comparison === '>') planet[column] > value;
-  //         switch (filter.comparison) {
-  //         case MAIOR_QUE:
-  //           return (planet[filter.column] > value);
-  //         case MENOR_QUE:
-  //           return planet[filter.column] < value;
-  //         case IGUAL_A:
-  //           return planet[filter.column] === value;
-  //         default:
-  //           console.log(`Erro no campo Comparison. Valor passado ${filter.comparison} ???`);
-  //           return planet;
-  //         }
-  //       });
-  //     }
-  //     // filteredListPlanets.push(result);
-  //   });
-  //   let newFilteredListPlanets = [];
-  //   filteredListPlanets.forEach((list) => {
-  //     newFilteredListPlanets = [...newFilteredListPlanets, ...list];
-  //   });
-  //   setFiltered(newFilteredListPlanets);
-  //   console.log(newFilteredListPlanets);
-  // }
 
-  // Planetas com x ><= Y
   function liCreator({ filterByNumericValues }) {
     const liList = filterByNumericValues.map((filter, index) => (
       <li key={ index }>
@@ -145,8 +55,12 @@ export default function Filter() {
     ));
     return liList;
   }
-  // Os filtros selecionados devem ser renderizados na tela depois do click.
 
+  // function optionColumnCreator() {
+
+  //   return Object.keys(data[0]).filter((option) => option !== 'films').map((item, index) => <option key={ index }>{item}</option>);
+  //   // return options;
+  // }
   return (
     <section>
       <form>
@@ -171,11 +85,11 @@ export default function Filter() {
           onChange={ ({ target }) => setColumn(target.value) }
         >
           <option disabled selected>Selecione um valor</option>
-          <option>population</option>
-          <option>orbital_period</option>
-          <option>diameter</option>
-          <option>rotation_period</option>
-          <option>surface_water</option>
+          {
+            columnsOptions.map(((option, index) => (
+              <option key={ index }>{option}</option>
+            )))
+          }
         </select>
 
         <select
