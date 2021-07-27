74083f30d5784d020e9260a44a47e751fa230907
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-07-27 15:48:46 -0300

    adiciona estilo do App

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import Provider from './utils/Provider';
 import Table from './components/Table';
 import Filters from './components/Filters';
 import AppliedFilters from './components/AppliedFilters';
+import style from './App.module.css';
 
 function App() {
   return (
     <Provider>
-      <Filters />
-      <AppliedFilters />
-      <Table />
+      <main className={ style.container }>
+        <header className={ style.header }>
+          <i className={ `fab fa-jedi-order ${style.icon}` } />
+          <h1 className={ style.title }>Starwars Planets</h1>
+          <i className={ `fab fa-jedi-order ${style.icon}` } />
+        </header>
+        <Filters />
+        <AppliedFilters />
+        <Table />
+      </main>
     </Provider>
   );
 }
