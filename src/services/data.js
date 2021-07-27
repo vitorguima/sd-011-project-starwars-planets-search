8d5b703b56981d9dff6f7ab1c87f52f0836c83f4
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-07-27 12:03:26 -0300

    fix: pasta service e arquivo data

--- src/services/data.js
+++ src/services/data.js
@@ -3,6 +3,6 @@ const URL = 'https://swapi-trybe.herokuapp.com/api/planets/';
 const getPlanets = async () => {
   const planetsData = await fetch(URL);
   return planetsData.json();
-}
+};
 
 export default getPlanets;
