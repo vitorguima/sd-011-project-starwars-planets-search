eda12d8b8bd1227ccb75ab2f34fbbe7c375687e8
Gustavo Lemes <gustavolemes997@gmail.com>
2021-07-27 20:18:04 -0300

    Requisito 3

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,21 +1,73 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 // import './App.css';
 import PlanetsContext from '../context/PlanetsContext';
 
 function Filters() {
   const {
     filterByName,
+    filterByNumericValues,
   } = useContext(PlanetsContext);
 
+  const [state, setState] = useState({ column: '', comparison: '', valor: '' });
+
+  const handleChange = ({ name, value }) => {
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
+  const { column, comparison, valor } = state;
+
   return (
-    <input
-      data-testid="name-filter"
-      type="text"
-      placeholder="Filtre um Nome"
-      onChange={ ({ target: { value } }) => {
-        filterByName(value);
-      } }
-    />
+    <div>
+      <input
+        data-testid="name-filter"
+        type="text"
+        placeholder="Filtre um Nome"
+        onChange={ ({ target: { value } }) => {
+          filterByName(value);
+        } }
+      />
+      <select
+        onChange={ ({ target }) => handleChange(target) }
+        name="column"
+        data-testid="column-filter"
+      >
+        <option defaultValue="selected">Selecione uma Opção</option>
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
+      <select
+        onChange={ ({ target }) => handleChange(target) }
+        name="comparison"
+        data-testid="comparison-filter"
+      >
+        <option defaultValue="selected">Selecione uma Opção</option>
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+      <input
+        onChange={ ({ target }) => handleChange(target) }
+        name="valor"
+        data-testid="value-filter"
+        type="number"
+        placeholder="Valor"
+      />
+      <button
+        onClick={ () => {
+          filterByNumericValues(column, comparison, valor);
+        } }
+        data-testid="button-filter"
+        type="button"
+      >
+        Filtrar
+      </button>
+    </div>
   );
 }
 
--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -8,7 +8,10 @@ function PlanetsProvider({ children }) {
   const [planetsData, setPlanetsData] = useState([]);
   const [loading, setSetLoading] = useState(false);
   const [error, setError] = useState(undefined);
-  const [filters, setFilters] = useState([]);
+  const [filters, setFilters] = useState([
+    { filterByName: {} },
+    { filterByNumericValues: [] },
+  ]);
 
   const handleAPISuccess = ({ results }) => {
     setAPIData(results);
@@ -38,8 +41,32 @@ function PlanetsProvider({ children }) {
     setFilters(updatedFilter);
 
     const filterPlanets = apiData.filter(({ name }) => (
-      name.includes(text)
+      name.toLowerCase().includes(text.toLowerCase())
     ));
+    setPlanetsData(filterPlanets);
+  };
+
+  const filterByNumericValues = (column, comparison, value) => {
+    const updatedFilter = filters;
+    updatedFilter[1].filterByNumericValues.push({
+      column,
+      comparison,
+      value,
+    });
+    setFilters(updatedFilter);
+
+    const filterPlanets = apiData.filter((planet) => {
+      if (comparison === 'maior que') {
+        return parseInt(planet[`${column}`], 10) > parseInt(value, 10);
+      }
+      if (comparison === 'menor que') {
+        return parseInt(planet[`${column}`], 10) < parseInt(value, 10);
+      }
+      if (comparison === 'igual a') {
+        return parseInt(planet[`${column}`], 10) === parseInt(value, 10);
+      }
+      return null;
+    });
     setPlanetsData(filterPlanets);
   };
 
@@ -52,6 +79,7 @@ function PlanetsProvider({ children }) {
           loading,
           error,
           filterByName,
+          filterByNumericValues,
         } }
       >
         {children}
