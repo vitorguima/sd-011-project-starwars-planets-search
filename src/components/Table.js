13bd412857498b00062e71ae4b3842ff6e2e83bb
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-07-27 13:22:36 -0300

    Refatorado 4, 5 e 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,21 +1,30 @@
 import React, { useContext } from 'react';
 import GlobalContext from '../context/GlobalContext';
 import TableInfos from './TableInfos';
-import { dropdownColumns, dropdownTag } from '../data/data';
+import { dropdownColumns, dropdownTag, planetInfos } from '../data/data';
 
 function Table() {
   const data = useContext(GlobalContext);
+
   const [dropdownFilter, setDropdownFilter] = React.useState({
     column: 'population',
     comparison: 'maior que',
     value: '',
   });
+
+  const [sort, setSort] = React.useState('ASC');
+  const [column, setColumn] = React.useState('name');
+
   const [filter, setFilter] = React.useState({
     filters: {
       filterByName: {
         name: '',
       },
       filterByNumericValues: [],
+      order: {
+        column: 'name',
+        sort: 'ASC',
+      },
     },
   });
 
@@ -38,20 +47,46 @@ function Table() {
   }
 
   function addNewFilter() {
-    const { column, comparison, value } = dropdownFilter;
+    const { column: col, comparison, value } = dropdownFilter;
     setFilter({
       filters: {
         ...filter.filters,
         filterByNumericValues: [
           ...filter.filters.filterByNumericValues,
-          { column, comparison, value },
+          { col, comparison, value },
         ],
       },
     });
   }
 
+  function removeFilter(index) {
+    setFilter({
+      filters: {
+        ...filter.filters,
+        filterByNumericValues: [
+          ...filter.filters.filterByNumericValues.filter(
+            (_, idx) => index !== idx,
+          ),
+        ],
+      },
+    });
+  }
+
+  function setOrder() {
+    setFilter({
+      filters: {
+        ...filter.filters,
+        order: {
+          sort,
+          column,
+        },
+      },
+    });
+  }
+
   if (!data) return <p>Loading...</p>;
-  const filteredByName = data.filter(({ name }) => name.includes(filter.filters.filterByName.name));
+  const filteredByName = data.filter(({ name }) => name
+    .includes(filter.filters.filterByName.name));
 
   const filteredByDropdown = filteredByName.filter((planet) => {
     const { filterByNumericValues: filters } = filter.filters;
@@ -59,28 +94,69 @@ function Table() {
     if (filters.length === 0) return true;
 
     const actualFilter = filters[filters.length - 1];
-    const { column, comparison, value } = actualFilter;
+    const { col, comparison, value } = actualFilter;
 
     switch (comparison) {
     case 'maior que':
-      return Number(planet[column]) > Number(value);
+      return Number(planet[col]) > Number(value);
     case 'menor que':
-      return Number(planet[column]) < Number(value);
+      return Number(planet[col]) < Number(value);
     case 'igual a':
-      return Number(planet[column]) === Number(value);
+      return Number(planet[col]) === Number(value);
     default:
       return true;
     }
   });
 
+  const { filterByNumericValues: filters, order } = filter.filters;
+  const { column: colmn, sort: sorted } = order;
+  const filteredDropdownColumns = dropdownColumns.filter(
+    (item) => !filters.some(({ col }) => col === item),
+  );
+
   return (
     <div>
+      <select
+        onChange={ ({ target: { value } }) => setColumn(value) }
+        data-testid="column-sort"
+      >
+        {['name', ...planetInfos].map((info, index) => (
+          <option key={ index }>{info}</option>
+        ))}
+      </select>
+
+      <label htmlFor="ASC">
+        ASC
+        <input
+          id="ASC"
+          data-testid="column-sort-input-asc"
+          name="sort"
+          type="radio"
+          onClick={ () => setSort('ASC') }
+        />
+      </label>
+
+      <label htmlFor="DESC">
+        DESC
+        <input
+          onChange={ () => setSort('DESC') }
+          id="DESC"
+          data-testid="column-sort-input-desc"
+          name="sort"
+          type="radio"
+        />
+      </label>
+
+      <button onClick={ setOrder } data-testid="column-sort-button" type="button">
+        Ordernar
+      </button>
+
       <select
         onChange={ changeDropdownFilter }
         name="column"
         data-testid="column-filter"
       >
-        {dropdownColumns.map((item, index) => (
+        {filteredDropdownColumns.map((item, index) => (
           <option key={ index }>{item}</option>
         ))}
       </select>
@@ -108,7 +184,22 @@ function Table() {
 
       <input type="text" data-testid="name-filter" onChange={ inputSearch } />
 
-      <TableInfos arrayOfPlanets={ filteredByDropdown } />
+      {filters.map((item, index) => (
+        <p data-testid="filter" key={ index }>
+          <span>{`${item.col} `}</span>
+          <span>{`${item.comparison} `}</span>
+          <span>{`${item.value} `}</span>
+          <button onClick={ () => removeFilter(index) } type="button">
+            x
+          </button>
+        </p>
+      ))}
+
+      <TableInfos
+        sort={ sorted }
+        column={ colmn }
+        arrayOfPlanets={ filteredByDropdown }
+      />
     </div>
   );
 }
--- src/components/TableInfos.js
+++ src/components/TableInfos.js
@@ -2,12 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { planetInfos } from '../data/data';
 
-function TableInfos({ arrayOfPlanets }) {
+function TableInfos({ arrayOfPlanets, column, sort }) {
   if (!arrayOfPlanets.length) return <p>Nenhum planeta encontrado</p>;
 
   const theads = Object.keys(arrayOfPlanets[0]).filter(
     (planetInfo) => planetInfo !== 'residents',
   );
+
+  arrayOfPlanets.sort((planetA, planetB) => {
+    if (sort === 'ASC' && /^[0-9]/.test(planetA[column])) {
+      return Number(planetA[column]) - Number(planetB[column]);
+    }
+
+    if (sort === 'ASC') {
+      return planetA[column].charCodeAt() - planetB[column].charCodeAt();
+    }
+
+    if (sort === 'DESC' && /^[0-9]/.test(planetA[column])) {
+      return Number(planetB[column]) - Number(planetA[column]);
+    }
+
+    return planetB[column].charCodeAt() - planetA[column].charCodeAt();
+  });
 
   return (
     <table>
@@ -37,4 +53,6 @@ export default TableInfos;
 
 TableInfos.propTypes = {
   arrayOfPlanets: PropTypes.arrayOf(PropTypes.object).isRequired,
+  sort: PropTypes.string.isRequired,
+  column: PropTypes.string.isRequired,
 };
