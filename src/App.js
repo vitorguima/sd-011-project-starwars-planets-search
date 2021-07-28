4bf1a156fc8138a2ff5f09919f76c306531c8778
Anajulia Brito <juliehindius@gmail.com>
2021-07-28 14:45:44 -0300

    Iniciando

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Table from './components/table';
 
-function App() {
+const App = () => {
+  const [planets, setPlanets] = useState([]);
+  useEffect(() => {
+    const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        setPlanets(data.results);
+      } catch (error) {
+        console.log('error', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <span>Hello, App!</span>
+    Table()
   );
-}
-
+//(alias) const App: () => JSX.Element
 export default App;
