17eb334b1c98e21327bd1eff320e282808502297
Thyago Pessoa <flash_thyago@hotmail.com>
2021-07-28 16:27:03 -0300

    Requisito 4 - refactoring

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,83 +1,16 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import MyContext from '../context/MyContext';
 import Search from './Search';
 
 function Table() {
-  const data = useContext(MyContext);
-  const [search, setSearch] = useState('');
-  const [planetsFiltered, setPlanetsFiltered] = useState([]);
-  const [random, setRandom] = useState({
-    column: 'population', comparison: '', value: '' });
-  const [columns, setColumn] = useState([]);
-  const [filters, setFilters] = useState({
-    filters: {
-      filterByName: {
-        name: '',
-      },
-      filterByNumericValues: [],
-    },
-  });
-
-  useEffect(() => {
-    if (data) setPlanetsFiltered(data);
-  }, [data]);
-
-  useEffect(() => {
-    setColumn([
-      'population',
-      'orbital_period',
-      'diameter',
-      'rotation_period',
-      'surface_water',
-    ]);
-  }, []);
-
-  if (data.length === 0) return <p>Loading</p>;
-
-  function toSearchInput({ target }) {
-    setSearch({
-      filters: {
-        ...filters.filters,
-        filterByName: { name: target.value } },
-    });
-    setPlanetsFiltered(data.filter((planet) => planet.name.includes(target.value)));
-  }
-
-  function randomState({ target }) {
-    const { name, value } = target;
-    setRandom({
-      ...random,
-      [name]: value,
-    });
-  }
-
-  const getRandom = planetsFiltered.filter((value) => {
-    const { filterByNumericValues } = filters.filters;
-
-    if (filterByNumericValues.length > 0) {
-      const randomValues = filterByNumericValues[filterByNumericValues.length - 1];
-      switch (randomValues.comparison) {
-      case 'maior que':
-        return Number(value[randomValues.column]) > Number(randomValues.value);
-      case 'menor que':
-        return Number(value[randomValues.column]) < Number(randomValues.value);
-      case 'igual a':
-        return Number(value[randomValues.column]) === Number(randomValues.value);
-      default: return true;
-      }
-    } return true;
-  }) || data;
-
-  function handleClickButton() {
-    const { column, comparison, value } = random;
-    setFilters({
-      filters: {
-        ...filters.filters.filterByName,
-        filterByNumericValues: [...filters.filters.filterByNumericValues,
-          { column, comparison, value }] },
-    });
-    setColumn(columns.filter((values) => values !== column));
-  }
+  const {
+    search,
+    columns,
+    toSearchInput,
+    randomState,
+    getRandom,
+    handleClickButton,
+  } = useContext(MyContext);
 
   return (
     <div>
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -5,6 +5,19 @@ import fetchApi from '../components/useFetchApi';
 
 function Provider({ children }) {
   const [data, setData] = useState('');
+  const [search, setSearch] = useState('');
+  const [planetsFiltered, setPlanetsFiltered] = useState([]);
+  const [random, setRandom] = useState({
+    column: 'population', comparison: '', value: '' });
+  const [columns, setColumn] = useState([]);
+  const [filters, setFilters] = useState({
+    filters: {
+      filterByName: {
+        name: '',
+      },
+      filterByNumericValues: [],
+    },
+  });
 
   async function fetchUseEffect() {
     setData(await fetchApi());
@@ -14,8 +27,78 @@ function Provider({ children }) {
     fetchUseEffect();
   }, []);
 
+  useEffect(() => {
+    if (data) setPlanetsFiltered(data);
+  }, [data, setPlanetsFiltered]);
+
+  useEffect(() => {
+    setColumn([
+      'population',
+      'orbital_period',
+      'diameter',
+      'rotation_period',
+      'surface_water',
+    ]);
+  }, [setColumn]);
+
+  if (data.length === 0) return <p>Loading</p>;
+
+  function toSearchInput({ target }) {
+    setSearch({
+      filters: {
+        ...filters.filters,
+        filterByName: { name: target.value } },
+    });
+    setPlanetsFiltered(data.filter((planet) => planet.name.includes(target.value)));
+  }
+
+  function randomState({ target }) {
+    const { name, value } = target;
+    setRandom({
+      ...random,
+      [name]: value,
+    });
+  }
+
+  const getRandom = planetsFiltered.filter((value) => {
+    const { filterByNumericValues } = filters.filters;
+
+    if (filterByNumericValues.length > 0) {
+      const randomValues = filterByNumericValues[filterByNumericValues.length - 1];
+      switch (randomValues.comparison) {
+      case 'maior que':
+        return Number(value[randomValues.column]) > Number(randomValues.value);
+      case 'menor que':
+        return Number(value[randomValues.column]) < Number(randomValues.value);
+      case 'igual a':
+        return Number(value[randomValues.column]) === Number(randomValues.value);
+      default: return true;
+      }
+    } return true;
+  });
+
+  function handleClickButton() {
+    const { column, comparison, value } = random;
+    setFilters({
+      filters: {
+        ...filters.filters.filterByName,
+        filterByNumericValues: [...filters.filters.filterByNumericValues,
+          { column, comparison, value }] },
+    });
+    setColumn(columns.filter((values) => values !== column));
+  }
+
+  const context = {
+    search,
+    columns,
+    toSearchInput,
+    randomState,
+    getRandom,
+    handleClickButton,
+  };
+
   return (
-    <MyContext.Provider value={ data }>
+    <MyContext.Provider value={ context }>
       {children}
     </MyContext.Provider>
   );
