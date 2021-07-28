aa5b12ef3476dadc73999d619db6b2ad3630ce96
Marcos Mantovani <mvmes23@gmail.com>
2021-07-28 02:29:03 -0300

    requerimento 3 finalizado

--- src/components/StarWarsProvider.js
+++ src/components/StarWarsProvider.js
@@ -5,9 +5,29 @@ import fetchPlanetsApi from '../services/StarWarsPlanetApi';
 
 function StarWarsProvider(props) {
   const [data, setData] = useState([]);
-  const [filters, setFilters] = useState({
-    filterByName: { name: '' },
-  });
+  const [planetName, setPlanetName] = useState('');
+  const [column, setColum] = useState('population');
+  const [comparison, setComparison] = useState('maior que');
+  const [value, setValue] = useState(0);
+  const [filterToApply, setFilterToApply] = useState(false);
+  const [FilteredResults, setFilteredResults] = useState([]);
+
+  const infos = {
+    data,
+    setData,
+    planetName,
+    setPlanetName,
+    column,
+    setColum,
+    comparison,
+    setComparison,
+    value,
+    setValue,
+    filterToApply,
+    setFilterToApply,
+    FilteredResults,
+    setFilteredResults,
+  };
 
   useEffect(() => {
     const getData = async () => {
@@ -15,11 +35,11 @@ function StarWarsProvider(props) {
       return setData(Data);
     };
     getData();
-  }, [filters.filterByName.name]);
+  }, [planetName]);
 
   const { children } = props;
   return (
-    <StarWarsContext.Provider value={ { setFilters, data, filters } }>
+    <StarWarsContext.Provider value={ { infos } }>
       { children }
     </StarWarsContext.Provider>
   );
--- src/components/Table.js
+++ src/components/Table.js
@@ -2,7 +2,14 @@ import React, { useContext } from 'react';
 import StarWarsContext from './StarWarsContext';
 
 function Table() {
-  const { data, setFilters, filters } = useContext(StarWarsContext);
+  const { infos } = useContext(StarWarsContext);
+  const { planetName, setPlanetName, data, column, setColum, comparison,
+    setComparison, value, setValue, filterToApply, setFilterToApply,
+    FilteredResults, setFilteredResults,
+  } = infos;
+
+  const filerByName = data.filter((item) => (
+    item.name.toLowerCase().includes(planetName.toLowerCase())));
 
   function getKeys() {
     if (data[0]) {
@@ -13,23 +20,89 @@ function Table() {
     }
   }
 
-  const filerByName = data.filter((item) => (
-    item.name.toLowerCase().includes(filters.filterByName.name.toLowerCase())));
-
-  const handleInputNameChange = ({ target }) => {
-    setFilters(
-      { ...filters, filterByName: { name: target.value } },
+  function columSelectRender() {
+    return (
+      <select
+        id="setColum"
+        data-testid="column-filter"
+        onChange={ (e) => setColum(e.target.value) }
+      >
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
     );
-  };
+  }
+
+  function comparisonSelectRender() {
+    return (
+      <select
+        id="setComparison"
+        data-testid="comparison-filter"
+        onChange={ (e) => setComparison(e.target.value) }
+      >
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+    );
+  }
+
+  function valueFieldInputRender() {
+    return (
+      <input
+        id="setValue"
+        type="number"
+        data-testid="value-filter"
+        onChange={ (e) => setValue(e.target.value) }
+      />
+    );
+  }
+
+  function filterBtnOnClickHandler() {
+    setFilterToApply(true);
+    if (comparison === 'maior que') {
+      const filteredByFilters = data
+        .filter((item) => Number(item[column]) > Number(value));
+      return setFilteredResults(filteredByFilters);
+    } if (comparison === 'menor que') {
+      const filteredByFilters = data
+        .filter((item) => Number(item[column]) < Number(value));
+      return setFilteredResults(filteredByFilters);
+    } if (comparison === 'igual a') {
+      const filteredByFilters = data
+        .filter((item) => Number(item[column]) === Number(value));
+      return setFilteredResults(filteredByFilters);
+    }
+  }
+
+  function applyFiltersBtnRender() {
+    return (
+      <div>
+        <button
+          data-testid="button-filter"
+          type="button"
+          onClick={ filterBtnOnClickHandler }
+        >
+          Add Filtros
+        </button>
+      </div>
+    );
+  }
 
   return (
     <>
       <input
         type="text"
         data-testid="name-filter"
-        value={ filters.filterByName.name }
-        onChange={ handleInputNameChange }
+        onChange={ (e) => setPlanetName(e.target.value) }
       />
+      { columSelectRender() }
+      { comparisonSelectRender() }
+      { valueFieldInputRender() }
+      { applyFiltersBtnRender() }
       <table>
         <thead>
           <tr>
@@ -38,11 +111,16 @@ function Table() {
         </thead>
         <tbody>
           {
-            filerByName.map((item, index) => (
+            !filterToApply ? filerByName.map((item, index) => (
               <tr key={ index }>
                 {Object.values(item).map((tdName, index2) => (
                   <td key={ index2 }>{tdName}</td>))}
-              </tr>))
+              </tr>)) : (
+              FilteredResults.map((item, index) => (
+                <tr key={ index }>
+                  {Object.values(item).map((tdName, index2) => (
+                    <td key={ index2 }>{tdName}</td>))}
+                </tr>)))
           }
         </tbody>
       </table>
