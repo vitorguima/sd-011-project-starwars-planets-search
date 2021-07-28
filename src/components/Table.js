803d964b6a4398ae9e1dd6e58852db8457e1aff7
IgorMendez <igor.emp@hotmail.com>
2021-07-28 08:26:59 -0300

    Exercício 2

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,34 +1,47 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import MyContext from '../context/MyContext';
 
 function Table() {
   const { data } = useContext(MyContext);
-  // console.log(data);
+  const [filterName, filteredName] = useState([]);
 
+  useEffect(() => {
+    filteredName();
+  }, []);
+  const planets1 = filterName ? data
+    .filter((planet) => planet.name.includes(filterName)) : data;
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          { data.map((planets) => Object.keys(planets)
-            .filter((el) => el !== 'residents')
-            .map((keysPlanets, index) => (
-              <th
-                key={ `key ${index}` }
-              >
-                {keysPlanets}
-              </th>)))[0] }
-        </tr>
-      </thead>
-      <tbody>
-        { data.map((planets, index) => (
-          <tr key={ `key ${index}` }>
-            { Object.entries(planets)
-              .filter((element) => !element.includes('residents'))
-              .map((planet, i) => (<td key={ `key ${i}` }>{planet[1]}</td>)) }
+    <div>
+      <label htmlFor="input">
+        <input
+          data-testid="name-filter"
+          onChange={ (e) => filteredName(e.target.value) }
+        />
+      </label>
+      <table className="table">
+        <thead>
+          <tr>
+            { data.map((planets) => Object.keys(planets)
+              .filter((el) => el !== 'residents')
+              .map((keysPlanets, index) => (
+                <th
+                  key={ `key ${index}` }
+                >
+                  {keysPlanets}
+                </th>)))[0] }
           </tr>
-        )) }
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          { planets1.map((planets, index) => (
+            <tr key={ `key ${index}` }>
+              { Object.entries(planets)
+                .filter((element) => !element.includes('residents'))
+                .map((planet, i) => (<td key={ `key ${i}` }>{planet[1]}</td>)) }
+            </tr>
+          )) }
+        </tbody>
+      </table>
+    </div>
   );
 }
 
