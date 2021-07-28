6a406a3fd276a7a4a96cee5e7236a98ea5abf644
gabiazevedo <gabiazevedoms@gmail.com>
2021-07-27 23:10:45 -0300

    Adiciona requisito 3 e 4

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Table from './components/Table';
 import './App.css';
 import PlanetProvider from './context/PlanetProvider';
 import FilterInput from './components/FilterInput';
+import Selectors from './components/Selectors';
 
 function App() {
   return (
     <PlanetProvider>
       <span className="title">🪐Star Wars Planets🪐</span>
       <FilterInput />
+      <Selectors />
       <Table />
     </PlanetProvider>
   );
--- src/context/PlanetProvider.js
+++ src/context/PlanetProvider.js
@@ -5,8 +5,18 @@ import PlanetContext from './PlanetContext';
 function PlanetProvider({ children }) {
   // meu estado;
   const [data, setData] = useState([]);
-  const [filters] = useState({ filterByName: { name: '' } });
   const [filterPlanet, setFilterPlanet] = useState([]);
+  const [filters, setFilters] = useState(
+    {
+      filterByName: {
+        name: '',
+      },
+      filterByNumericValues: [],
+    },
+  );
+
+  const { filterByName, filterByNumericValues } = filters;
+  const { name } = filterByName;
 
   useEffect(() => {
     const endpointPlanets = 'https://swapi-trybe.herokuapp.com/api/planets/';
@@ -21,13 +31,43 @@ function PlanetProvider({ children }) {
 
   const filterPlanets = ({ target }) => {
     const { value } = target;
-    const planetsFiltereds = data.filter((planet) => planet.name
-      .toLowerCase().includes(value));
-    setFilterPlanet(planetsFiltereds);
+    setFilters({ ...filters, filterByName: { name: value } });
   };
 
+  useEffect(() => {
+    console.log('entrou no effect');
+    let planetsFiltereds = data.filter((planet) => planet.name
+      .toLowerCase().includes(name));
+    if (filterByNumericValues.length > 0) {
+      planetsFiltereds = planetsFiltereds.filter((planet) => {
+        let bool = true;
+        filterByNumericValues.forEach(({ column, comparison, value }) => {
+          switch (comparison) {
+          case 'maior que':
+            bool = Number(planet[column]) > Number(value) && bool;
+            break;
+          case 'menor que':
+            bool = Number(planet[column]) < Number(value) && bool;
+            break;
+          case 'igual a':
+            bool = Number(planet[column]) === Number(value) && bool;
+            break;
+          default:
+            bool = false;
+          }
+        });
+        return bool;
+      });
+    }
+    setFilterPlanet(planetsFiltereds);
+  }, [data, name, filters, filterByNumericValues]);
+
+  const setNumericFilters = (newObj) => {
+    setFilters({ ...filters,
+      filterByNumericValues: [...filters.filterByNumericValues, newObj] });
+  };
   // meu contexto com informações para passar pros próximos componentes
-  const context = { data, filterPlanets, filters, filterPlanet };
+  const context = { data, filterPlanets, filters, filterPlanet, name, setNumericFilters };
   // retornando meu contexto.provider e passando todas as infos para componentes filhos
   return (
     <PlanetContext.Provider value={ context }>
