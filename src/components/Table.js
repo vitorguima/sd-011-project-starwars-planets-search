e396cbb2be946c816af926bb83d829367e9dad1f
Daniel Leon <daniel.radialista@gmail.com>
2021-07-28 21:34:15 -0300

    requisito 3, pronto

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,58 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import ContextPlanetsApi from '../context/ContextPlanetsApi';
 
 function Table() {
-  const { namePlanets, filterPlanets } = useContext(ContextPlanetsApi);
+  const { namePlanets, options, filters, setFilters, column, setColumn,
+    comparison, setComparison, value, setValue } = useContext(ContextPlanetsApi);
+  const [nameFilter, setNameFilter] = useState('');
+  const [columnFilter, setColumnFilter] = useState('population');
+  const [comparisonFilter, setComparisonFilter] = useState('maior que');
+  const [valueFilter, setValueFilter] = useState(0);
+
+  const filterByName = nameFilter ? namePlanets
+    .filter((planet) => planet.name.includes(nameFilter)) : namePlanets;
+
+  const filterByHigherThan = namePlanets
+    .filter((planet) => planet[column] > Number(value));
+
+  const filterByLessThan = namePlanets
+    .filter((planet) => planet[column] < Number(value));
+
+  const filterByEqual = namePlanets
+    .filter((planet) => Number(planet[columnFilter]) === Number(value));
+
+  const renderPlanets = () => {
+    if (nameFilter) {
+      return filterByName;
+    }
+    if (comparison === 'maior que') {
+      return filterByHigherThan;
+    }
+    if (comparison === 'menor que') {
+      return filterByLessThan;
+    }
+    if (comparison === 'igual a') {
+      return filterByEqual;
+    }
+    return namePlanets;
+  };
+
+  function handleClick() {
+    setColumn(columnFilter);
+    setComparison(comparisonFilter);
+    setValue(valueFilter);
+    const objectNumbers = {
+      column: columnFilter,
+      comparison: comparisonFilter,
+      value: valueFilter,
+    };
+    setFilters({
+      ...filters,
+      filterByNumericValues:
+        [...filters.filterByNumericValues, objectNumbers],
+    });
+  }
+
   return (
     <section>
       <form>
@@ -11,9 +61,40 @@ function Table() {
           <input
             type="text"
             data-testid="name-filter"
-            onChange={ filterPlanets }
+            onChange={ ({ target }) => {
+              setNameFilter(target.value); setFilters({
+                ...filters,
+                filterByName: { name: target.value },
+              });
+            } }
           />
         </label>
+        <select
+          data-testid="column-filter"
+          onChange={ ({ target }) => setColumnFilter(target.value) }
+        >
+          {options.map((option, index) => <option key={ index }>{option}</option>)}
+        </select>
+        <select
+          data-testid="comparison-filter"
+          onChange={ ({ target }) => setComparisonFilter(target.value) }
+        >
+          <option>maior que</option>
+          <option>menor que</option>
+          <option>igual a</option>
+        </select>
+        <input
+          type="number"
+          data-testid="value-filter"
+          onChange={ ({ target }) => setValueFilter(target.value) }
+        />
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ handleClick }
+        >
+          Filtrar
+        </button>
       </form>
       <table>
         <thead>
@@ -34,7 +115,7 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-          {namePlanets.map((planet, index) => (
+          {renderPlanets().map((planet, index) => (
             <tr key={ index }>
               <td>{planet.name}</td>
               <td>{planet.rotation_period}</td>
--- src/context/ProviderPlanets.js
+++ src/context/ProviderPlanets.js
@@ -6,7 +6,18 @@ import ContextPlanetsApi from './ContextPlanetsApi';
 function ProviderPlanets({ children }) {
   const [planets, setPlanets] = useState([]);
   const [namePlanets, setNamePlanets] = useState([]);
+  const [filters, setFilters] = useState({
+    filterByName: {
+      name: '',
+    },
+    filterByNumericValues: [],
+  });
+  const [options, setOptions] = useState(['population', 'orbital_period', 'diameter',
+    'rotation_period', 'surface_water']);
 
+  const [column, setColumn] = useState('');
+  const [comparison, setComparison] = useState('');
+  const [value, setValue] = useState(0);
   useEffect(() => {
     getPlanets().then(({ results }) => {
       results.forEach((result) => delete result.residents);
@@ -15,17 +26,30 @@ function ProviderPlanets({ children }) {
     });
   }, []);
 
-  const filterPlanets = ({ target: { value } }) => {
-    const inputFindPlanet = planets.filter(({ name }) => (
-      name.toLowerCase().includes(value.toLowerCase())
-    ));
-    setNamePlanets(inputFindPlanet);
-  };
+  useEffect(() => {
+    const filterPlanets = () => {
+      const { filterByName } = filters;
+      const inputFindPlanet = planets.filter(({ name }) => (
+        name.toLowerCase().includes(filterByName.name.toLowerCase())
+      ));
+      setNamePlanets(inputFindPlanet);
+    };
+    filterPlanets();
+  }, [planets, filters]);
 
   const allContexts = {
     planets,
     namePlanets,
-    filterPlanets,
+    options,
+    setOptions,
+    filters,
+    setFilters,
+    column,
+    setColumn,
+    comparison,
+    setComparison,
+    value,
+    setValue,
   };
 
   return (
