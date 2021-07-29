18796a5e7f5245ac29fb095090335d965e4d7534
IgorMendez <igor.emp@hotmail.com>
2021-07-28 22:08:55 -0300

    pausa para o lanche

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,21 +3,75 @@ import MyContext from '../context/MyContext';
 
 function Table() {
   const { data } = useContext(MyContext);
-  const [filterName, filteredName] = useState([]);
+  const [filters, filteredObj] = useState({ filterNumValues: 'population' });
+  const [filtersNam, filtered] = useState();
+  const [arrayFiltered, submitSearch] = useState([]);
+  const arrayDropDown = [
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ];
 
   useEffect(() => {
-    filteredName();
+    filtered();
   }, []);
-  const planets1 = filterName ? data
-    .filter((planet) => planet.name.includes(filterName)) : data;
+  // console.log(filtersNam);
+
+  // console.log(arrayFiltered);
+
+  console.log(filters);
+
+  const planets1 = data.filter((planet) => (filtersNam ? planet.name.toLowerCase()
+    .includes(filtersNam.filterName.toLowerCase()) : data));
   return (
     <div>
       <label htmlFor="input">
         <input
           data-testid="name-filter"
-          onChange={ (e) => filteredName(e.target.value) }
+          onChange={ (e) => filtered({ ...filtersNam, filterName: e.target.value }) }
         />
       </label>
+      <form method="GET">
+        <select
+          data-testid="column-filter"
+          onChange={ (e) => filteredObj({ ...filters, filterNumValues: e.target.value }) }
+        >
+          { arrayDropDown.map((elements, index) => (
+            <option
+              className="optionss"
+              value={ elements }
+              key={ `key${index}` }
+            >
+              { elements }
+            </option>
+          )) }
+        </select>
+        <select
+          data-testid="comparison-filter"
+          onChange={ (e) => filteredObj({
+            ...filters, filterComparision: e.target.value }) }
+        >
+          <option className="comparision">maior que</option>
+          <option className="comparision">igual a</option>
+          <option className="comparision">menor que</option>
+        </select>
+        <label htmlFor="input">
+          <input
+            type="number"
+            data-testid="value-filter"
+            onChange={ (e) => filteredObj({ ...filters, filterNumber: e.target.value }) }
+          />
+        </label>
+        <button
+          type="button"
+          value="GET"
+          onClick={ () => submitSearch(data.filter((e) => e)) }
+        >
+          Pesquisar
+        </button>
+      </form>
       <table className="table">
         <thead>
           <tr>
