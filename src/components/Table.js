a51fd62535df999320706159bea8b0481b43cf47
IgorMendez <igor.emp@hotmail.com>
2021-07-29 02:09:55 -0300

    Exercício 3 resolvido

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,8 @@ import MyContext from '../context/MyContext';
 
 function Table() {
   const { data } = useContext(MyContext);
-  const [filters, filteredObj] = useState({ filterNumValues: 'population', filterComparision: 'maior que' });
+  const [filters, filteredObj] = useState({ filterNumValues: 'population',
+    comp: 'maior que' });
   const [filtersNam, filtered] = useState();
   const [arrayFiltered, submitSearch] = useState([]);
   const arrayDropDown = [
@@ -13,36 +14,28 @@ function Table() {
     'rotation_period',
     'surface_water',
   ];
-
   const planets1 = data.filter((planet) => (filtersNam ? planet.name.toLowerCase()
     .includes(filtersNam.filterName.toLowerCase()) : data));
-
-  const setData = ({ filterNumValues, filterComparision }) => {
-    const data2 = data.filter((e, i, a) => {
-      if (filterComparision === 'maior que' && parseInt(e[filterNumValues], 10) > 0) {
+  const setData = ({ filterNumValues, comp }) => {
+    const data2 = data.filter((e) => {
+      if (comp === 'maior que' && parseInt(e[filterNumValues], 10) !== null) {
         return parseInt(e[filterNumValues], 10) > parseInt((filters.filterNumber), 10);
       }
-      if (filterComparision === 'igual a' && parseInt(e[filterNumValues], 10) >= 0) {
+      if (comp === 'igual a' && parseInt(e[filterNumValues], 10) !== null) {
         return parseInt(e[filterNumValues], 10) === parseInt((filters.filterNumber), 10);
       }
-      if (filterComparision === 'menor que' && parseInt(e[filterNumValues], 10) >= 0) {
+      if (comp === 'menor que' && parseInt(e[filterNumValues], 10) !== null) {
         return parseInt(e[filterNumValues], 10) < parseInt((filters.filterNumber), 10);
       }
-
+      return data;
     });
-    // console.log(data2);
     return submitSearch({ ...arrayFiltered, arr: data2 });
   };
 
   useEffect(() => {
-    // submitSearch(planets1)
     filtered();
   }, []);
-  // console.log(arrayFiltered);
   const compare = arrayFiltered.arr ? arrayFiltered.arr : planets1;
-
-  // console.log(arrayFiltered);
-
   return (
     <div>
       <label htmlFor="input">
@@ -51,55 +44,49 @@ function Table() {
           onChange={ (e) => filtered({ ...filtersNam, filterName: e.target.value }) }
         />
       </label>
-      <form method="GET">
-        <select
-          data-testid="column-filter"
-          onChange={ (e) => filteredObj({ ...filters, filterNumValues: e.target.value }) }
-        >
-          { arrayDropDown.map((elements, index) => (
-            <option
-              className="optionss"
-              value={ elements }
-              key={ `key${index}` }
-            >
-              { elements }
-            </option>
-          )) }
-        </select>
-        <select
-          data-testid="comparison-filter"
-          onChange={ (e) => filteredObj({
-            ...filters, filterComparision: e.target.value }) }
-        >
-          <option>maior que</option>
-          <option>igual a</option>
-          <option>menor que</option>
-        </select>
-        <label htmlFor="input">
-          <input
-            type="number"
-            data-testid="value-filter"
-            onChange={ (e) => filteredObj({ ...filters, filterNumber: e.target.value }) }
-          />
-        </label>
-        <button
-          data-testid="button-filter"
-          type="button"
-          value="GET"
-          onClick={ () => setData(filters) }
-        >
-          Pesquisar
-        </button>
-      </form>
+      <select
+        data-testid="column-filter"
+        onChange={ (e) => filteredObj({ ...filters, filterNumValues: e.target.value }) }
+      >
+        { arrayDropDown.map((elements, index) => (
+          <option
+            className="optionss"
+            value={ elements }
+            key={ `key${index}` }
+          >
+            { elements }
+          </option>
+        )) }
+      </select>
+      <select
+        data-testid="comparison-filter"
+        onChange={ (e) => filteredObj({ ...filters, comp: e.target.value }) }
+      >
+        <option>maior que</option>
+        <option>igual a</option>
+        <option>menor que</option>
+      </select>
+      <label htmlFor="input">
+        <input
+          type="number"
+          data-testid="value-filter"
+          onChange={ (e) => filteredObj({ ...filters, filterNumber: e.target.value }) }
+        />
+      </label>
+      <button
+        data-testid="button-filter"
+        type="button"
+        onClick={ () => setData(filters) }
+      >
+        Pesquisar
+      </button>
       <table className="table">
         <thead>
           <tr>
             { data.map((planets) => Object.keys(planets)
               .filter((el) => el !== 'residents')
               .map((keysPlanets, index) => (
-                <th
-                  key={ `key ${index}` }
-                >
+                <th key={ `key ${index}` }>
                   {keysPlanets}
                 </th>)))[0] }
           </tr>
@@ -117,5 +104,4 @@ function Table() {
     </div>
   );
 }
-
 export default Table;
