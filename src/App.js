411b1e8a61b0f13da23074bb7468bc99ba950d1a
Maran <maranmorimoto@gmail.com>
2021-07-29 11:59:27 -0300

    request 02 and 03 done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import PlanetsProvider from './context/PlanetsProvider';
-import Table from './Table';
+import Table from './components/Table';
+import FilterPlanets from './components/FilterPlanets';
 
 function App() {
   return (
     <PlanetsProvider>
+      <FilterPlanets />
       <Table />
     </PlanetsProvider>
   );
--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -4,7 +4,16 @@ import getPlanets from '../services/starwarsPlanetsAPI';
 import Context from './Context';
 
 export default function PlanetsProvider({ children }) {
+  const initialStateFilters = {
+    filterByName: {
+      name: '',
+    },
+    filterByNumericValues: [],
+  };
+
   const [data, setData] = useState([]);
+  const [filters, setFilters] = useState(initialStateFilters);
+  const [filteredPlanets, setFilteredPlanets] = useState([]);
 
   useEffect(() => {
     const getDataFromAPI = async () => {
@@ -14,7 +23,42 @@ export default function PlanetsProvider({ children }) {
     getDataFromAPI();
   }, []);
 
-  const info = { data };
+  useEffect(() => {
+    const showPlanets = () => {
+      const planetFilteredName = data
+        .filter((planet) => planet.name.includes(filters.filterByName.name));
+
+      if (filters.filterByNumericValues.length === 0) {
+        setFilteredPlanets(planetFilteredName);
+      } else {
+        const { filterByNumericValues } = filters;
+        const { column, comparison, value } = filterByNumericValues[0];
+        const planetFilterNumber = planetFilteredName.filter((planet) => {
+          if (comparison === 'maior que') {
+            return Number(planet[`${column}`]) > Number(value);
+          } if (comparison === 'igual a') {
+            return Number(planet[`${column}`]) === Number(value);
+          }
+          return Number(planet[`${column}`]) < Number(value);
+        });
+        setFilteredPlanets(planetFilterNumber);
+      }
+    };
+    showPlanets();
+  },
+  [data,
+    filters.filterByName.name,
+    filters.filterByNumericValues,
+    filters, setFilteredPlanets]);
+
+  const info = {
+    data,
+    setData,
+    filters,
+    setFilters,
+    filteredPlanets,
+    setFilteredPlanets,
+  };
 
   return (
     <Context.Provider value={ info }>
