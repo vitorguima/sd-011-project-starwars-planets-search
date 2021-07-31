3f9967d34c5fcea69ecb18ed86e29011413a6352
Henrique_Pozzolini <hpmoreira05@gmail.com>
2021-07-31 20:01:47 -0300

    Requisito 3 e 4

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,75 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PlanetContext from '../context/planetContext';
 
 function Table() {
-  const { planets, isLoading, keys, filters, setFilters } = useContext(PlanetContext);
+  const {
+    isLoading,
+    keys,
+    setFilters,
+    filters,
+    filteredByNumber,
+    arrayPlanets,
+  } = useContext(PlanetContext);
+  const [localFilter, setLocalFilter] = useState({
+    column: 'population',
+    comparison: 'maior que',
+    value: '',
+  });
+
+  function submit() {
+    setFilters({ ...filters,
+      filterByNumericValues: [...filters.filterByNumericValues, localFilter],
+    });
+    const column = document.getElementById('column-filter');
+    const option = document.getElementById(`${localFilter.column}`);
+    column.removeChild(option);
+  }
+
+  useEffect(() => {
+    filteredByNumber();
+  }, [filters]);
 
   return (
     <div>
       <input
         data-testid="name-filter"
         type="text"
-        onChange={ (e) => setFilters({ filterByName: {
-          name: e.target.value,
-        } }) }
+        onChange={ (e) => setFilters({ ...filters,
+          filterByName: {
+            name: e.target.value,
+          } }) }
       />
+      <select
+        data-testid="column-filter"
+        id="column-filter"
+        onChange={ (e) => setLocalFilter({ ...localFilter, column: e.target.value }) }
+      >
+        <option value="population" id="population">population</option>
+        <option value="orbital_period" id="orbital_period">orbital_period</option>
+        <option value="diameter" id="diameter">diameter</option>
+        <option value="rotation_period" id="rotation_period">rotation_period</option>
+        <option value="surface_water" id="surface_water">surface_water</option>
+      </select>
+      <select
+        data-testid="comparison-filter"
+        onChange={ (e) => setLocalFilter({ ...localFilter, comparison: e.target.value }) }
+      >
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+      <input
+        type="number"
+        data-testid="value-filter"
+        onChange={ (e) => setLocalFilter({ ...localFilter, value: e.target.value }) }
+      />
+      <button
+        type="button"
+        onClick={ submit }
+        data-testid="button-filter"
+      >
+        Filtrar
+      </button>
       <table>
         <thead>
           <tr>
@@ -20,7 +77,7 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-          {!isLoading && planets.results
+          {!isLoading && arrayPlanets
             .filter((planet) => planet.name.includes(filters.filterByName.name))
             .map((planet, index) => (
               <tr key={ index }>
--- src/context/planetProvider.js
+++ src/context/planetProvider.js
@@ -6,9 +6,11 @@ function PlanetProvider({ children }) {
   const [planets, setPlanets] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [keys, setKeys] = useState([]);
+  const [arrayPlanets, setArrayPlanets] = useState([]);
   const [filters, setFilters] = useState({ filterByName: {
     name: '',
-  } });
+  },
+  filterByNumericValues: [] });
 
   async function fetchPlanets() {
     const request = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
@@ -16,8 +18,52 @@ function PlanetProvider({ children }) {
     const data = response;
     const header = Object.keys(response.results[0]).filter((key) => key !== 'residents');
     setPlanets(data);
+    setArrayPlanets(data.results);
     setIsLoading(false);
     setKeys(header);
+  }
+
+  // function filterNames() {
+  //   const filteredNames = planets.results
+  //     .filter((planet) => planet.name.includes(filters.filterByName.name));
+  //   return filteredNames;
+  // }
+
+  // function filteredByNumber() {
+  //     if (filters.filterByNumericValues[0].comparison === "maior que") {
+  //     return filterNames().filter((planet) => parseInt(planet[`${filters.filterByNumericValues[0].column}`]) > `${filters.filterByNumericValues[0].value}`);
+  //     } if (filters.filterByNumericValues[0].comparison === "menor que") {
+  //     return filterNames().filter((planet) => parseInt(planet[`${filters.filterByNumericValues[0].column}`]) < `${filters.filterByNumericValues[0].value}`);
+  //     } if (filters.filterByNumericValues[0].comparison === "igual a") {
+  //     return filterNames().filter((planet) => parseInt(planet[`${filters.filterByNumericValues[0].column}`]) === `${filters.filterByNumericValues[0].value}`);
+  //     }
+  // }
+
+  function filteredByNumber() {
+    if (filters.filterByNumericValues.length > 0) {
+      filters.filterByNumericValues.forEach((element) => {
+        if (element.comparison === 'maior que') {
+          setArrayPlanets(arrayPlanets
+            .filter(
+              (planet) => parseInt(planet[`${element.column}`], 10) > `${element.value}`,
+            ));
+          return arrayPlanets;
+        } if (element.comparison === 'menor que') {
+          setArrayPlanets(arrayPlanets
+            .filter(
+              (planet) => parseInt(planet[`${element.column}`], 10) < `${element.value}`,
+            ));
+          return arrayPlanets;
+        } if (element.comparison === 'igual a') {
+          setArrayPlanets(arrayPlanets
+            .filter(
+              (planet) => parseInt(planet[`${element.column}`], 10)
+              === parseInt(`${element.value}`, 10),
+            ));
+          return arrayPlanets;
+        }
+      });
+    }
   }
 
   useEffect(() => {
@@ -26,7 +72,14 @@ function PlanetProvider({ children }) {
 
   return (
     <PlanetContext.Provider
-      value={ { planets, isLoading, keys, fetchPlanets, filters, setFilters } }
+      value={ { planets,
+        isLoading,
+        keys,
+        fetchPlanets,
+        filters,
+        setFilters,
+        filteredByNumber,
+        arrayPlanets } }
     >
       {children}
     </PlanetContext.Provider>
