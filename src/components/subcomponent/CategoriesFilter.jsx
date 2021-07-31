09c4d3707f0691eed8d54261ba547475bb2fa466
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-07-31 13:46:56 -0300

    test - corrigindo testId

--- src/components/subcomponent/CategoriesFilter.jsx
+++ src/components/subcomponent/CategoriesFilter.jsx
@@ -26,11 +26,11 @@ function CategoriesFilter() {
           getOnChangeInputs(false);
         } }
       >
-        <option selected value="surface_water">Surface water</option>
-        <option value="diameter">Diameter</option>
-        <option value="population">Population</option>
-        <option value="orbital_period">Orbital period</option>
-        <option value="rotation_period">Rotation period</option>
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
       </select>
       <select
         data-testid="comparison-filter"
@@ -39,7 +39,7 @@ function CategoriesFilter() {
           getOnChangeInputs(false);
         } }
       >
-        <option selected value="maior que">maior que</option>
+        <option value="maior que">maior que</option>
         <option value="menor que">menor que</option>
         <option value="igual a">igual a</option>
       </select>
