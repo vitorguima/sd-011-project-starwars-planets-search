52bedad539f71d11f064801583c5551d2cf899a9
Alan Gonçalves <vekio.soft@gmail.com>
2021-08-01 21:26:52 -0300

    conclusão requisito 3

--- src/context/StarProvider.js
+++ src/context/StarProvider.js
@@ -5,6 +5,10 @@ import StarContext from './StarContext';
 
 function PlanetsProvider({ children }) {
   const [data, setData] = useState([]);
+  const [filters, setFilters] = useState([
+    { filterByName: {} },
+    { filterByNumericValues: [] },
+  ]);
 
   useEffect(() => {
     const getDataFromAPI = async () => {
@@ -14,8 +18,32 @@ function PlanetsProvider({ children }) {
     getDataFromAPI();
   }, []);
 
+  const filterByNumericValues = (column, comparison, value) => {
+    const updatedFilter = filters;
+    updatedFilter[1].filterByNumericValues.push({
+      column,
+      comparison,
+      value,
+    });
+    setFilters(updatedFilter);
+
+    const filterPlanets = data.filter((planet) => {
+      if (comparison === 'maior que') {
+        return parseInt(planet[`${column}`], 10) > parseInt(value, 10);
+      }
+      if (comparison === 'menor que') {
+        return parseInt(planet[`${column}`], 10) < parseInt(value, 10);
+      }
+      if (comparison === 'igual a') {
+        return parseInt(planet[`${column}`], 10) === parseInt(value, 10);
+      }
+      return null;
+    });
+    setData(filterPlanets);
+  };
+
   return (
-    <StarContext.Provider value={ { data } }>
+    <StarContext.Provider value={ { data, filterByNumericValues } }>
       { children }
     </StarContext.Provider>
   );
