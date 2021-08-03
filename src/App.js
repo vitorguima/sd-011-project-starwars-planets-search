761ea8f62a83fa0a9db4fe3517b1c4ebce5c9564
Matheus Costa <mathws.costag@gmail.com>
2021-08-03 03:33:07 -0300

    Requisito 4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import AppContext from './Context';
 import Table from './Components/Table';
 import Search from './Components/Search';
@@ -8,7 +8,6 @@ import './App.css';
 function App() {
   const [data, setData] = useState();
   const [list, setList] = useState();
-
   useEffect(() => {
     async function planets() {
       const planetList = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
@@ -19,6 +18,28 @@ function App() {
     planets();
   }, []);
 
+  const filterFunc = useCallback(
+    ({ column, comparison, value }) => {
+      const updateList = data.reduce((acc, cur) => {
+        const curValue = parseInt(cur[column], 10);
+        const filterValue = parseInt(value, 10);
+        switch (comparison) {
+        case 'maior que':
+          return (curValue > filterValue) ? [...acc, cur] : acc;
+        case 'menor que':
+          return (curValue < filterValue) ? [...acc, cur] : acc;
+        case 'igual a':
+          return (curValue === filterValue) ? [...acc, cur] : acc;
+        default:
+          return [...acc, cur];
+        }
+      }, []);
+      setList(updateList);
+    },
+    [data],
+  );
+
+  const [filters, setFilter] = useState([]);
   const [name, setName] = useState('');
   useEffect(() => {
     if (data) {
@@ -29,9 +50,15 @@ function App() {
         return acc;
       }, []));
     }
-  }, [name, data, setList]);
+    if (filters.length !== 0) {
+      filters.map((element) => filterFunc(element));
+    }
+  }, [name, data, setList, filters, filterFunc]);
+
   return (
-    <AppContext.Provider value={ { data, setData, list, setList, setName } }>
+    <AppContext.Provider
+      value={ { data, setData, list, setList, setName, setFilter, filters } }
+    >
       <h1>Star Wars Planets</h1>
       <Search />
       <SearchByNumber />
--- src/Components/SearchByNumber.jsx
+++ src/Components/SearchByNumber.jsx
@@ -1,52 +1,34 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import AppContext from '../Context';
 
 const initialValue = <div>initialValue</div>;
+const initialColumns = ['population',
+  'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
 
 export default function SearchByNumber() {
-  const { data, setList } = useContext(AppContext);
+  const [columns, setColumns] = useState(initialColumns);
+  const { filters, setFilter } = useContext(AppContext);
   const columnFilter = useRef(initialValue);
   const comparisonFilter = useRef(initialValue);
   const valueFilter = useRef(initialValue);
 
-  function filterFunc() {
-    const list = data.reduce((acc, cur) => {
-      const value = parseInt(cur[columnFilter.current.value], 10);
-      const filterValue = parseInt(valueFilter.current.value, 10);
-      switch (comparisonFilter.current.value) {
-      case 'maior que':
-        return (value > filterValue) ? (
-          [...acc, cur]
-        ) : (
-          acc
-        );
-      case 'menor que':
-        return (value < filterValue) ? (
-          [...acc, cur]
-        ) : (
-          acc
-        );
-      case 'igual a':
-        return (value === filterValue) ? (
-          [...acc, cur]
-        ) : (
-          acc
-        );
-      default:
-        return [...acc, cur];
+  function setNewFilter(obj) {
+    setFilter([...filters, obj]);
+    const newColumns = columns.reduce((acc, cur) => {
+      if (cur === obj.column) {
+        return acc;
       }
+      return [...acc, cur];
     }, []);
-    setList(list);
+    setColumns(newColumns);
   }
 
   return (
     <div>
       <select data-testid="column-filter" ref={ columnFilter }>
-        <option value="population">population</option>
-        <option value="orbital_period">orbital_period</option>
-        <option value="diameter">diameter</option>
-        <option value="rotation_period">rotation_period</option>
-        <option value="surface_water">surface_water</option>
+        { columns.map((element, index) => (
+          <option value={ element } key={ index }>{ element }</option>
+        )) }
       </select>
       <select data-testid="comparison-filter" ref={ comparisonFilter }>
         <option value="maior que">maior que</option>
@@ -57,7 +39,11 @@ export default function SearchByNumber() {
       <button
         type="button"
         data-testid="button-filter"
-        onClick={ () => filterFunc() }
+        onClick={ () => setNewFilter({
+          column: columnFilter.current.value,
+          comparison: comparisonFilter.current.value,
+          value: valueFilter.current.value,
+        }) }
       >
         Adicionar
       </button>
