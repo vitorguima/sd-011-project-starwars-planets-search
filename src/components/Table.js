3c2252f1a5e8b6675be1a010630ee9b237a4f685
camiloLelis <lafirmafuerte@gmail.com>
2021-08-03 11:10:01 -0300

    requisito-2-tentat-1

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,47 +1,84 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import starWarsContex from '../context/starWarsContex';
 
 export default function Table() {
   const data = useContext(starWarsContex);
+  const [filters, setfilters] = useState({
+    filterByName: {
+      name: '',
+    },
+  });
+
+  const [filtered, setfitered] = useState([]);
+
+  useEffect(() => {
+    function filterData() {
+      let newArray = data;
+      if (newArray.length > 0 && filters.filterByName.name) {
+        newArray = newArray
+          .filter((item) => item.name.toLowerCase()
+            .includes(filters.filterByName.name.toLowerCase()));
+      }
+      setfitered(newArray);
+    }
+    filterData();
+  },
+  [data, filters]);
+
+  const handleInputName = ({ target: { value } }) => {
+    setfilters({
+      ...filters,
+      filterByName: {
+        name: value,
+      },
+    });
+  };
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>climate</th>
-          <th>created</th>
-          <th>diameter</th>
-          <th>edited</th>
-          <th>films</th>
-          <th>gravity</th>
-          <th>name</th>
-          <th>orbital_period</th>
-          <th>population</th>
-          <th>rotation_period</th>
-          <th>surgace_water</th>
-          <th>terrain</th>
-          <th>url</th>
-        </tr>
-      </thead>
-      {data.map((planet, index) => (
-        <tbody key={ index }>
-          <tr key={ index }>
-            <td>{ planet.climate }</td>
-            <td>{ planet.created }</td>
-            <td>{ planet.diameter }</td>
-            <td>{ planet.edited }</td>
-            <td>{ planet.films.map((film, i) => (<p key={ i }>{ film }</p>))}</td>
-            <td>{ planet.gravity }</td>
-            <td>{ planet.name }</td>
-            <td>{ planet.orbital_period }</td>
-            <td>{ planet.population }</td>
-            <td>{ planet.rotation_period }</td>
-            <td>{ planet.surface_water }</td>
-            <td>{ planet.terrain }</td>
-            <td>{ planet.url }</td>
-            { console.log(data) }
+    <>
+      <input
+        type="text"
+        onChange={ (e) => handleInputName(e) }
+        data-testid="name-filter"
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>climate</th>
+            <th>created</th>
+            <th>diameter</th>
+            <th>edited</th>
+            <th>films</th>
+            <th>gravity</th>
+            <th>name</th>
+            <th>orbital_period</th>
+            <th>population</th>
+            <th>rotation_period</th>
+            <th>surgace_water</th>
+            <th>terrain</th>
+            <th>url</th>
           </tr>
-        </tbody>
-      ))}
-    </table>
+        </thead>
+        {filtered.map((planet, index) => (
+          <tbody key={ index }>
+            <tr key={ index }>
+              <td>{ planet.climate }</td>
+              <td>{ planet.created }</td>
+              <td>{ planet.diameter }</td>
+              <td>{ planet.edited }</td>
+              <td>{ planet.films.map((film, i) => (<p key={ i }>{ film }</p>))}</td>
+              <td>{ planet.gravity }</td>
+              <td>{ planet.name }</td>
+              <td>{ planet.orbital_period }</td>
+              <td>{ planet.population }</td>
+              <td>{ planet.rotation_period }</td>
+              <td>{ planet.surface_water }</td>
+              <td>{ planet.terrain }</td>
+              <td>{ planet.url }</td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
+    </>
   );
 }
