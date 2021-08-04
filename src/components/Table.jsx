812defacde8c2238a942ed833f04be9f315346ae
Michelle <mls.srbek@gmail.com>
2021-08-04 10:56:20 -0300

    form with num filter and clear button

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,17 +5,41 @@ export default function Table() {
   const { planetsAPI } = useContext(PlanetsContext);
   const [planets, setPlanets] = useState();
   const [filters, setFilters] = useState({
-    filters:
-    { filterByName: { name: '' } },
+    filters: {
+      filterByName: { name: '' },
+    },
   });
+  const [numFilters, setNumFilters] = useState([]);
+
+  const [column, setColumn] = useState('population');
+  const [comparison, setComparison] = useState('maior que');
+  const magicNumber = 100000;
+  const [value, setValue] = useState(magicNumber);
+
   const header = ['Name', 'Rotation', 'Orbital Period',
     'Diameter', 'Climate', 'Gravity',
     'Terrain', 'Surface Water', 'Population',
     'Films', 'Created', 'Edited', 'Url'];
+  const columns = ['population', 'orbital_period', 'diameter',
+    'rotation_period', 'surface_water'];
 
   function handleInputPlanet({ target }) {
     setFilters({ filters: { filterByName: { name: target.value } } });
-    console.log(filters);
+    console.log(numFilters);
+  }
+
+  function handleNumFilters() {
+    const filter = [...numFilters];
+    filter.push({ column, comparison, value });
+    setNumFilters(filter);
+  }
+
+  function handleClear() {
+    setFilters({ filters: { filterByName: { name: '' } } });
+    setNumFilters([]);
+    setColumn('population');
+    setComparison('maior que');
+    setValue(magicNumber);
   }
 
   useEffect(() => {
@@ -36,6 +60,51 @@ export default function Table() {
           placeholder="Filter by name"
         />
       </label>
+      <form>
+        <label htmlFor="column">
+          Filter by:
+          <select
+            onChange={ (event) => setColumn(event.target.value) }
+            data-testid="column-filter"
+          >
+            { columns.map((col, i) => (
+              <option key={ i } value={ col }>{ col }</option>)) }
+          </select>
+        </label>
+        <label htmlFor="comparison">
+          Compare by:
+          <select
+            onChange={ (event) => setComparison(event.target.value) }
+            data-testid="comparison-filter"
+          >
+            <option value="">Select your option</option>
+            <option value="maior que">maior que</option>
+            <option value="menor que">menor que</option>
+            <option value="igual a">igual a</option>
+          </select>
+        </label>
+        <label htmlFor="value">
+          Value:
+          <input
+            type="number"
+            onChange={ (event) => setValue(event.target.value) }
+            data-testid="value-filter"
+          />
+        </label>
+        <button
+          type="button"
+          onClick={ handleNumFilters }
+          data-testid="button-filter"
+        >
+          Apply Filters
+        </button>
+        <button
+          type="button"
+          onClick={ handleClear }
+        >
+          X
+        </button>
+      </form>
       <table>
         <thead>
           <tr className="tableHead">
