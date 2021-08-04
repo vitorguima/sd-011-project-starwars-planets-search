be7545a439a91e65ce5b9eaddaa3b64b4a9a0c21
FredericoCanoeira <fcanoeira@gmail.com>
2021-08-04 16:52:41 +0100

    implement requiriments 3,4 and 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,106 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import AppContext from '../context/Context';
 
 const SearchBar = () => {
-  const { filterByName } = useContext(AppContext);
+  const {
+    filterByName,
+    filters,
+    setFilters,
+    deleteFilter,
+    filterByNumericValues } = useContext(AppContext);
+  const [filtersLocal, setFiltersLocal] = useState({
+    column: 'population',
+    comparison: 'maior que',
+    value: 'null',
+  });
+  const [dropdown, setdropdrown] = useState([
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ]);
   const handleChange = ({ target: { value } }) => {
     filterByName(value);
   };
 
+  const handleFilter = ({ target: { name, value } }) => {
+    setFiltersLocal({
+      ...filtersLocal,
+      [name]: value,
+    });
+  };
+
+  const handleButton = (e) => {
+    e.preventDefault();
+    filterByNumericValues(filtersLocal);
+    setdropdrown(dropdown.filter((item) => item !== filtersLocal.column));
+    setFilters([
+      ...filters,
+      filtersLocal,
+    ]);
+  };
   return (
-    <section>
-      <input
-        type="text"
-        name="searchText"
-        id="searchText"
-        onChange={ handleChange }
-        data-testid="name-filter"
-        className="input"
-      />
-    </section>
+    <form>
+      <section>
+        <input
+          type="text"
+          name="searchText"
+          id="searchText"
+          onChange={ handleChange }
+          data-testid="name-filter"
+          className="input"
+        />
+        <select
+          data-testid="column-filter"
+          name="column"
+          onChange={ handleFilter }
+          required
+        >
+          {dropdown.map((item) => <option key={ item }>{item}</option>)}
+        </select>
+        <select
+          data-testid="comparison-filter"
+          name="comparison"
+          onChange={ handleFilter }
+          required
+        >
+          <option>maior que</option>
+          <option>menor que</option>
+          <option>igual a</option>
+        </select>
+        <input
+          type="number"
+          name="value"
+          data-testid="value-filter"
+          onChange={ handleFilter }
+          required
+        />
+        <button
+          data-testid="button-filter"
+          type="submit"
+          onClick={ handleButton }
+        >
+          Filtrar
+        </button>
+        <div>
+          {filters.map(({ column, comparison, value }, index) => (
+            <>
+              <span key={ column }>
+                { `${column} ${comparison} ${value}`}
+              </span>
+              <button
+                data-testid="filter"
+                type="button"
+                onClick={ () => deleteFilter(index) }
+              >
+                X
+              </button>
+            </>
+          ))}
+        </div>
+      </section>
+    </form>
   );
 };
 
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -5,6 +5,7 @@ import getApi from '../services/api';
 
 function Provider({ children }) {
   const [data, setData] = useState([]);
+  const [filters, setFilters] = useState([]);
   const [filteredPlanets, setFilteredPlanets] = useState([]);
   useEffect(() => {
     getApi().then((response) => setData(response));
@@ -22,11 +23,34 @@ function Provider({ children }) {
     setFilteredPlanets(filtered);
   };
 
+  const deleteFilter = (index) => {
+    const delFilter = filters.filter((_, indexFilter) => index !== indexFilter);
+    setFilters(delFilter);
+  };
+
+  const filterPlanetsByNumericValues = ({ column, comparison, value }) => {
+    const newFilter = data.filter((planet) => {
+      const infoPlanet = Number(planet[column]);
+      const toCompare = Number(value);
+      if (comparison === 'menor que') {
+        return infoPlanet < toCompare;
+      }
+      if (comparison === 'maior que') {
+        return infoPlanet > toCompare;
+      }
+      return infoPlanet === toCompare;
+    });
+    setFilteredPlanets(newFilter);
+  };
   const contextMainValue = {
     data,
     filteredPlanets,
+    deleteFilter,
+    setFilters,
     filterByName: (textName) => filterPlanetsByName(textName),
+    filterByNumericValues: (filtersLocal) => filterPlanetsByNumericValues((filtersLocal)),
   };
+
   return (
     <ContextMain.Provider value={ contextMainValue }>
       {children}
