c3d2c36950241539697718e3401bd0ae2e9b6032
FredericoCanoeira <fcanoeira@gmail.com>
2021-08-06 01:55:54 +0100

    fix slint II

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Provider from './context/Provider';
+import { Provider } from './context/Provider';
 import Table from './components/Table';
 
 function App() {
