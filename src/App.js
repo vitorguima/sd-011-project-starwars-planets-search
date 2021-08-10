b5f90c8f3629df7facf9c1c5c0c7b06334eef7a4
Lais Simenikim <l.simenikim@gmail.com>
2021-08-10 20:28:04 -0300

    add requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Table from './Table';
+import PlanetsContext from './PlanetsContext';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(undefined);
+
+  useEffect(() => {
+    fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result.results);
+      });
+  }, []);
+
   return (
-    <span>Hello, App!!</span>
+    <PlanetsContext.Provider value={ { data } }>
+      <Table />
+    </PlanetsContext.Provider>
   );
 }
 
---
+++ src/PlanetsContext.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const PlanetsContext = createContext({ data: undefined });
+
+export default PlanetsContext;
