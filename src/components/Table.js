a0159a61238a053a0738989ff07e8768d325f53a
tomsantos07 <xotmsantosx@live.com>
2021-08-11 01:54:37 -0300

    corrigindo loop infinito

--- src/components/Table.js
+++ src/components/Table.js
@@ -32,7 +32,7 @@ function Table() {
 
   useEffect(() => {
     setFilterPlanets(filterData(planets));
-  }, [filterData, planets, searchPlanet]);
+  }, [planets]);
 
   useEffect(() => {
     const getPlanets = async () => {
@@ -46,11 +46,10 @@ function Table() {
 
   useEffect(() => {
     filterPlanet();
-  }, [filter, filterPlanet]);
+  }, [filter]);
 
   return (
-    <table>
-
+    <>
       {
         searchPlanet ? (
           <button
@@ -61,45 +60,49 @@ function Table() {
             X
           </button>) : ''
       }
-      <thead>
-        <tr>
-          <th>Nome</th>
-          <th>Período de Rotação</th>
-          <th>Período Orbital</th>
-          <th>Diâmetro</th>
-          <th>Clima</th>
-          <th>Gravidade</th>
-          <th>Terreno</th>
-          <th>Água da Superfície</th>
-          <th>População</th>
-          <th>Filmes</th>
-          <th>Criado</th>
-          <th>Editado</th>
-          <th>URL</th>
-        </tr>
-      </thead>
+      <table>
 
-      <tbody>
-        {filterPlanets.map((planet) => (
-          <tr key={ planet.name }>
-            <td>{planet.name}</td>
-            <td>{planet.rotation_period}</td>
-            <td>{planet.orbital_period}</td>
-            <td>{planet.diameter}</td>
-            <td>{planet.climate}</td>
-            <td>{planet.gravity}</td>
-            <td>{planet.terrain}</td>
-            <td>{planet.surface_water}</td>
-            <td>{planet.population}</td>
-            <td>{planet.films}</td>
-            <td>{planet.created}</td>
-            <td>{planet.edited}</td>
-            <td>{planet.url}</td>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Período de Rotação</th>
+            <th>Período Orbital</th>
+            <th>Diâmetro</th>
+            <th>Clima</th>
+            <th>Gravidade</th>
+            <th>Terreno</th>
+            <th>Água da Superfície</th>
+            <th>População</th>
+            <th>Filmes</th>
+            <th>Criado</th>
+            <th>Editado</th>
+            <th>URL</th>
           </tr>
-        ))}
-      </tbody>
+        </thead>
 
-    </table>
+        <tbody>
+          {filterPlanets.map((planet) => (
+            <tr key={ planet.name }>
+              <td>{planet.name}</td>
+              <td>{planet.rotation_period}</td>
+              <td>{planet.orbital_period}</td>
+              <td>{planet.diameter}</td>
+              <td>{planet.climate}</td>
+              <td>{planet.gravity}</td>
+              <td>{planet.terrain}</td>
+              <td>{planet.surface_water}</td>
+              <td>{planet.population}</td>
+              <td>{planet.films}</td>
+              <td>{planet.created}</td>
+              <td>{planet.edited}</td>
+              <td>{planet.url}</td>
+            </tr>
+          ))}
+        </tbody>
+
+      </table>
+    </>
+
   );
 }
 
