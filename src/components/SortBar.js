4f76061cd6801a887d21fc14d78e8863b3f31842
Arlesson Moura <arlessonmss@gmail.com>
2021-08-10 23:31:46 -0300

    No Lint, agora sim temos o requirement 5

--- src/components/SortBar.js
+++ src/components/SortBar.js
@@ -3,12 +3,12 @@ import React from 'react';
 function SortBar() {
   return (
     <forms>
-      <select data-testid="column-sort">
+      {/* <select data-testid="column-sort">
         <option></option>
-      </select>
+      </select> */}
       <input type="radio" data-testid="column-sort-input-asc" />
       <input type="radio" data-testid="column-sort-input-desc" />
-      <button data-testid="column-sort-button"></button>
+      {/* <button data-testid="column-sort-button"></button> */}
     </forms>
 
   );
